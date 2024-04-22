b32a79b78990d360a09aec15292434977721254f
Noah Sherwin <noahrsherwin@gmail.com>
2024-04-22 02:31:57 -0700

    chore: fix prettier import

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,5 @@
 // https://www.npmjs.com/package/prettier-config-moon
-import pcm from 'prettier-config-moon/prettier.json'
+import pcm from 'prettier-config-moon' with { type: 'json' };
 export default {
 	...pcm,
 	...{
