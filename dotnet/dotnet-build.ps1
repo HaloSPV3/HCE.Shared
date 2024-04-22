0961d59c7feb24e925e79609d5453138c66509f0
Noah Sherwin <noahrsherwin@gmail.com>
2024-04-22 05:24:02 -0700

    feat(dotnet): add dotnet-build.ps1
    - intended for use by ./.github/workflows/_dotnet-build.yml, but can run locally.
    - build Debug and Release in parallel!
    - specify Solution files (sln, slnf, slnx) or Project (.csproj, .fsproj, .proj) files!
    - if no files provided via args or env var, will search recursively for Solutions!
    - if no Solutions found, will search recursively for Projects!
    - searches exclude common ignores! (.git, bin, node_modules, obj, publish)

---
+++ dotnet/dotnet-build.ps1
@@ -0,0 +1,66 @@
+# input: SLN_OR_PROJ_FILES
+# Comma-separated. May be relative or absolute.
+
+[System.IO.FileInfo[]]$solutions += ($env:SLN_OR_PROJ_FILES -split ';' | ForEach-Object { Get-Item $_ });
+if ($args.Count -gt 0) {
+    $args | ForEach-Object {
+        $solutions += ($args[0] -split ';' | ForEach-Object { Get-Item $_ })
+    }
+}
+
+$excludedDirs = '.git', 'bin', 'node_modules', 'obj', 'publish';
+$slnExts = '.sln', '.slnf', '.slnx';
+
+function RecursivelySearchForSolutions {
+    $result;
+
+    # get solutions (*.sln, *.slnf, *.slnx, etc) -in $rootItems
+    $solutions += (Get-ChildItem -File |
+        Where-Object { $_.Extension.ToLowerInvariant() -in $slnExts } |
+        Sort-Object |
+        Get-Unique
+    );
+
+    # also check in sub-directories
+    $subdirs = Get-ChildItem -Path $rootDir -Directory;
+    for ($i = 0; $i -lt $subdirs.Count; $i++) {
+        if (-not ($subdirs[$i].Name -in $excludedDirs)) {
+            $subdirs += (
+                Get-ChildItem -Path $curDir -Directory |
+                Where-Object { -not ($_.Name -in $excludedDirs) }
+            );
+        }
+    }
+    $solutions += (
+        $subdirs |
+        ForEach-Object {
+            Get-ChildItem -Path $_ -File |
+            Where-Object {
+                $_.Extension.ToLowerInvariant() -in $slnExts
+            }
+        }
+    );
+}
+
+# search for solutions files
+if ($solutions.Count -eq 0) {
+    RecursivelySearchForSolutions
+}
+
+# search for project files
+if ($solutions.Count -eq 0 ) {
+    $slnExts += ('.csproj', '.fsproj', '.proj');
+    RecursivelySearchForSolutions
+}
+
+if ($solutions.Count -eq 0) {
+    throw [System.IO.FileNotFoundException]::new('No Solution files (.sln, .slnf, .slnx) or Project files (.csproj, .fsproj, .proj) could be found!')
+}
+
+# solutions/projects are build sequentially, but Debug/Release variants are built in parallel
+$solutions | ForEach-Object {
+    $sln = $_;
+    'Debug', 'Release' | ForEach-Object -Parallel {
+        dotnet build $sln --configuration $_
+    }
+}
