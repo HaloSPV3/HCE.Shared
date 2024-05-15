4fe3cc273f5944959a3e7e76b8e2adb78d7b276b
Noah Sherwin <noahrsherwin@gmail.com>
2024-05-14 19:39:04 -0700

    build: prune rollup dynamic config

--- packemon.config.js
+++ packemon.config.js
@@ -1,13 +1,10 @@
 import babelConfig from "./babel.config.mjs"
-import packageJson from "./package.json" with {type: "json"}
-
-const deps = packageJson.dependencies;
 
 /** 
  * @typedef {import("./packemon.config.d.ts").ConfigFile} ConfigFile
  * @type {ConfigFile}
  */
-const config = {
+export default {
     /** @type {ConfigFile["babelInput"]} */
     babelInput(config) {
 
@@ -32,28 +29,5 @@ const config = {
             config.sourceRoot = "./src"
 
         // console.log(JSON.stringify(config, undefined, 2).replace(/"([^(")"]+)":/g, "$1:"));
-    },
-    // babelOutput(config, build) { },
-    /** @type {ConfigFile["rollupInput"]} */
-    rollupInput(config) {
-        config.shimMissingExports = true;
-        if (config.external === undefined || typeof config.external === "undefined")
-            config.external = [];
-        if (typeof config.external === "string" || typeof config.external === "object" && !Array.isArray(config.external))
-            config.external = [config.external];
-        if (Array.isArray(config.external)
-            && !config.external.includes('resolve')
-            && Object.keys(deps).includes("resolve")) {
-            config.external.push('resolve');
-        }
-        else {
-            /* Do nothing. It's a function. */
-        }
-    },
-    // rollupOutput(config, build) { },
-    swc: undefined,
-    // swcInput(config) { },
-    // swcOutput(config, build) { }
-}
-
-export default config;
+    }
+};
