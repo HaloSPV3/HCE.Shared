4413dff9f6cb41290dbd52c0a7c2135151a7244a
Noah Sherwin <noahrsherwin@gmail.com>
2024-05-21 20:03:55 -0700

    test: fix commitlintConfig tests
    missing declarations

--- tests/commitlintConfig.test.ts
+++ tests/commitlintConfig.test.ts
@@ -1,15 +1,15 @@
 import { deepStrictEqual, ok } from 'node:assert';
-import { describe, test } from 'node:test';
+import { describe, it } from 'node:test';
+// @ts-expect-error ts(7016) Could not find a declaration file
 import createPreset from 'conventional-changelog-conventionalcommits';
 import commitLintConventional from '@commitlint/config-conventional';
 import load from '@commitlint/load';
 import { type QualifiedRuleConfig, RuleConfigSeverity } from '@commitlint/types';
 import rawConfig from '@halospv3/hce.shared-config/commitlintConfig';
 
-
 await describe("commitlintConfig", async () => {
-	await test('Raw config tests', async (t) => {
-		await t.test('extends is defined', () => {
+	await describe("rawConfig", async () => {
+		await it("has extends", () => {
 			if (typeof rawConfig.extends === 'string')
 				deepStrictEqual(rawConfig.extends, '@commitlint/config-conventional');
 			else if (Array.isArray(rawConfig.extends))
@@ -17,8 +17,7 @@ await describe("commitlintConfig", async () => {
 			else ok(false, 'extends is not defined in config file or is an unsupported type!');
 		});
 
-
-		await t.test('rules is defined', () => {
+		await it("has rules", () => {
 			ok(rawConfig.rules);
 			const rules = rawConfig.rules;
 			deepStrictEqual(
@@ -36,108 +35,27 @@ await describe("commitlintConfig", async () => {
 		});
 	});
 
-	const config = await load(rawConfig);
-	await test('Loaded config tests', async (t) => {
-		await t.test('parserPreset is defined', async () => {
-			const preset = await createPreset({});
+	await describe('Loaded config tests', async () => {
+		const config = await load(rawConfig);
+		await it('has parserPreset', async () => {
+			const preset = await createPreset();
 			ok(config.parserPreset);
 			deepStrictEqual(config.parserPreset.name, commitLintConventional.parserPreset);
-			deepStrictEqual(config.parserPreset.parserOpts, preset.parserOpts);
+			deepStrictEqual(config.parserPreset.parserOpts, preset.parser);
 		});
-		await t.test('rules is defined', async (t_) => {
-			await t_.test(
-				'body-leading-blank',
-				() => {
-					deepStrictEqual(
-						config.rules['body-leading-blank'],
-						commitLintConventional.rules['body-leading-blank'],
-					);
-				},
-			);
-			await t_.test(
-				'body-max-line-length',
-				() => {
-					deepStrictEqual(
-						config.rules['body-max-line-length']?.[0],
-						RuleConfigSeverity.Disabled,
-					);
-				},
-			);
-			await t_.test(
-				'footer-leading-blank',
-				() => {
-					deepStrictEqual(
-						config.rules['footer-leading-blank'],
-						commitLintConventional.rules['footer-leading-blank'],
-					);
-				},
-			);
-			await t_.test(
-				'footer-max-line-length',
-				() => {
-					deepStrictEqual(
-						config.rules['footer-max-line-length']?.[0],
-						RuleConfigSeverity.Disabled,
-					);
-				},
-			);
-			await t_.test(
-				'header-max-length',
-				() => { deepStrictEqual(config.rules['header-max-length']?.[0], RuleConfigSeverity.Disabled); },
-			);
-			await t_.test(
-				'header-trim',
-				() => {
-					deepStrictEqual(
-						config.rules['header-trim'],
-						commitLintConventional.rules['header-trim'],
-					);
-				},
-			);
-			await t_.test(
-				'subject-case',
-				() => {
-					deepStrictEqual(
-						config.rules['subject-case'],
-						commitLintConventional.rules['subject-case'],
-					);
-				},
-			);
-			await t_.test(
-				'subject-empty',
-				() => {
-					deepStrictEqual(
-						config.rules['subject-empty'],
-						commitLintConventional.rules['subject-empty'],
-					);
-				},
-			);
-			await t_.test(
-				'subject-full-stop',
-				() => {
-					deepStrictEqual(
-						config.rules['subject-full-stop'],
-						commitLintConventional.rules['subject-full-stop'],
-					);
-				},
-			);
-			await t_.test(
-				'type-case',
-				() => { deepStrictEqual(config.rules['type-case'], commitLintConventional.rules['type-case']); },
-			);
-			await t_.test(
-				'type-empty',
-				() => {
-					deepStrictEqual(
-						config.rules['type-empty'],
-						commitLintConventional.rules['type-empty'],
-					);
-				},
-			);
-			await t_.test(
-				'type-enum',
-				() => { deepStrictEqual(config.rules['type-enum'], commitLintConventional.rules['type-enum']); },
-			);
+		await it('has rules', async () => {
+			deepStrictEqual(config.rules['body-leading-blank'], commitLintConventional.rules['body-leading-blank']);
+			deepStrictEqual(config.rules['body-max-line-length']?.[0], RuleConfigSeverity.Disabled);
+			deepStrictEqual(config.rules['footer-leading-blank'], commitLintConventional.rules['footer-leading-blank']);
+			deepStrictEqual(config.rules['footer-max-line-length']?.[0], RuleConfigSeverity.Disabled);
+			deepStrictEqual(config.rules['header-max-length']?.[0], RuleConfigSeverity.Disabled);
+			deepStrictEqual(config.rules['header-trim'], commitLintConventional.rules['header-trim']);
+			deepStrictEqual(config.rules['subject-case'], commitLintConventional.rules['subject-case']);
+			deepStrictEqual(config.rules['subject-empty'], commitLintConventional.rules['subject-empty']);
+			deepStrictEqual(config.rules['subject-full-stop'], commitLintConventional.rules['subject-full-stop']);
+			deepStrictEqual(config.rules['type-case'], commitLintConventional.rules['type-case']);
+			deepStrictEqual(config.rules['type-empty'], commitLintConventional.rules['type-empty']);
+			deepStrictEqual(config.rules['type-enum'], commitLintConventional.rules['type-enum']);
 		});
 	});
 })
