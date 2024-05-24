fdb6c7c0ecdccc41416f29b3e382cc3bdc3ff855
Noah Sherwin <noahrsherwin@gmail.com>
2024-05-24 07:02:12 -0700

    test: update semanticReleaseConfigDotnet tests
    All functions are now poorly tested. Better than nothing!

--- tests/configDotnet.test.ts
+++ tests/configDotnet.test.ts
@@ -1,14 +1,48 @@
-import { describe, todo } from 'node:test';
+import { describe, it } from 'node:test';
 import { appendPlugins, getConfig, insertAndEditPlugins } from '@halospv3/hce.shared-config/semanticReleaseConfigDotnet';
+import { notDeepStrictEqual, ok, strictEqual } from 'node:assert';
 
 await describe('configDotnet', async () => {
-	await todo(insertAndEditPlugins.name, (t) => {
-		t.todo('Insert/splice modifies the config');
-		t.todo('Plugins are inserted at the correct index');
-		t.todo('Git plugin options are added only when the Git plugin is already in array');
+	await describe('appendPlugins', () => {
+		strictEqual(appendPlugins.name, 'appendPlugins');
+
+		it('throws Error when Options parameters lacks "plugins"', () => {
+			let x = undefined;
+			try {
+				x = appendPlugins({}, [], [])
+			}
+			catch (error) {
+				strictEqual(error instanceof Error, true)
+			}
+			strictEqual(x, undefined)
+		});
 	});
 
-	await todo(appendPlugins.name);
+	await describe('insertAndEditPlugins', () => {
+		strictEqual(insertAndEditPlugins.name, 'insertAndEditPlugins');
 
-	await todo(getConfig.name);
+		it('modifies the Options object when expected to do so', () => {
+			const x = {};
+			const y = insertAndEditPlugins({ ...x })
+			notDeepStrictEqual(x, y);
+		});
+		it('inserts plugins at the correct positions', { todo: true });
+		it('Git plugin options are added only when the Git plugin is already in array', { todo: true });
+	});
+
+	await describe('getConfig', async () => {
+		strictEqual(getConfig.name, 'getConfig');
+
+		await it('throws Error when projectsToPublish, projectsToPackAndPush are both empty arrays.', () => {
+			const actual = (() => {
+				try {
+					return getConfig([], []);
+				} catch (error) {
+					return error as Error;
+				}
+			})();
+			ok(actual instanceof Error)
+			strictEqual(actual.message, "Type of projectsToPublish (object) is not allowed. Expected a string[] where length > 0.")
+		})
+	});
 });
