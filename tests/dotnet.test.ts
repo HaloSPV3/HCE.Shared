bfd769e32dabc3346c42981a8e4a07d329a959bc
Noah Sherwin <noahrsherwin@gmail.com>
2024-05-24 07:05:35 -0700

    test: wrap dotnet re-exporter's tests in "describe"
    It was causing connor4312.nodejs-testing to fail to differentiate between the re-exporter and the subfolder of the same name.

--- tests/dotnet.test.ts
+++ tests/dotnet.test.ts
@@ -1,23 +1,26 @@
 import * as dotnet from "@halospv3/hce.shared-config/dotnet" // see a ts(2307) here? It's probably safe to ignore!
 import { ok } from "assert/strict"
+import { describe } from "node:test"
 
 // simple "symbol exists" checks only. More specific checks in each module's respective test file.
 
-ok("getGithubNugetRegistryPair" in dotnet.dotnetGHPR)
-ok("nugetGitHubUrl" in dotnet.dotnetGHPR)
-ok("nugetGitHubUrlBase" in dotnet.dotnetGHPR)
+describe('dotnet re-export checks', () => {
+    ok("getGithubNugetRegistryPair" in dotnet.dotnetGHPR)
+    ok("nugetGitHubUrl" in dotnet.dotnetGHPR)
+    ok("nugetGitHubUrlBase" in dotnet.dotnetGHPR)
 
-ok("GitLabTokenEnvVar" in dotnet.dotnetGLPR)
-ok("getGitlabNugetRegistryPair" in dotnet.dotnetGLPR)
-ok("nugetGitLabUrl" in dotnet.dotnetGLPR)
-ok("nugetGitLabUrlBase" in dotnet.dotnetGLPR)
+    ok("GitLabTokenEnvVar" in dotnet.dotnetGLPR)
+    ok("getGitlabNugetRegistryPair" in dotnet.dotnetGLPR)
+    ok("nugetGitLabUrl" in dotnet.dotnetGLPR)
+    ok("nugetGitLabUrlBase" in dotnet.dotnetGLPR)
 
-ok("configureDotnetNugetPush" in dotnet.dotnetHelpers)
-ok("configurePrepareCmd" in dotnet.dotnetHelpers)
-ok("nugetDefault" in dotnet.dotnetHelpers)
+    ok("configureDotnetNugetPush" in dotnet.dotnetHelpers)
+    ok("configurePrepareCmd" in dotnet.dotnetHelpers)
+    ok("nugetDefault" in dotnet.dotnetHelpers)
 
-ok("MSBuildProjectPreDefinedProperties" in dotnet.msbuildProject)
-ok("MSBuildEvaluatedProjects" in dotnet.msbuildProject)
-ok("MSBuildProject" in dotnet.msbuildProject)
+    ok("MSBuildProjectPreDefinedProperties" in dotnet.msbuildProject)
+    ok("MSBuildEvaluatedProjects" in dotnet.msbuildProject)
+    ok("MSBuildProject" in dotnet.msbuildProject)
 
-ok("MSBuildProjectProperties" in dotnet.msbuildProjectProperties)
+    ok("MSBuildProjectProperties" in dotnet.msbuildProjectProperties)
+});
