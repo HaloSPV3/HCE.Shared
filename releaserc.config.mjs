e9ef24c158bc5f7bf0a0f865acaf92a9fef326b9
Noah Sherwin <noahrsherwin@gmail.com>
2024-05-30 07:34:53 -0700

    chore: exploit recent plugin-tuple change for semantic-release plugin config

--- releaserc.config.mjs
+++ releaserc.config.mjs
@@ -15,53 +15,71 @@
  * When 'plugins' is set, only commit-analyzer is added if missing.
  */
 
-/**
- * @type {import('@semantic-release/git').Options}
+import hceSharedConfig from '@halospv3/hce.shared-config'
+import { ok } from 'node:assert/strict';
+
+ok(hceSharedConfig.plugins)
+
+// #region COMMIT ANALYZER
+const COMMIT_ANALYZER_ID = '@semantic-release/commit-analyzer';
+const commitAnalyzerIndex = hceSharedConfig.plugins.findIndex(pluginSpec =>
+	pluginSpec === COMMIT_ANALYZER_ID || pluginSpec[0] === COMMIT_ANALYZER_ID
+);
+
+/** 
+ * @type {import('semantic-release').PluginSpec<import('@semantic-release/commit-analyzer').CommitAnalyzerConfig>} 
+ * https://github.com/semantic-release/commit-analyzer#options
  */
-const gitOptions = {
-	assets: [
-		'README.md',
-		'CHANGELOG.md',
-		'package.json',
-		'package-lock.json'
-	]
-};
+let commitAnalyzer = hceSharedConfig.plugins[commitAnalyzerIndex];
+if (typeof commitAnalyzer === 'string')
+	commitAnalyzer = [COMMIT_ANALYZER_ID, {}];
+
+/* Set Release Rules */
+
+let releaseRules = commitAnalyzer[1].releaseRules;
+if (releaseRules === undefined)
+	releaseRules = [];
+if (releaseRules instanceof String)
+	// @ts-expect-error Yes, it's a string. Stop complaining.
+	releaseRules = await import(commitAnalyzer[1].releaseRules)
+if (releaseRules === undefined || typeof releaseRules === 'string')
+	throw TypeError("it's supposed to be a string[] now");
+
+// description: https://github.com/semantic-release/commit-analyzer#releaserules
+// defaults: https://github.com/semantic-release/commit-analyzer/blob/master/lib/default-release-rules.js
+releaseRules.push(
+	{
+		type: '(build|chore|fix)',
+		scope: 'deps',
+		release: 'patch',
+	}
+)
+commitAnalyzer[1].releaseRules = releaseRules;
+// @ts-expect-error Index signature in type 'readonly PluginSpec<any>[]' only permits reading. ts(2542)
+hceSharedConfig.plugins[commitAnalyzerIndex] = commitAnalyzer;
+
+// #endregion COMMIT ANALYZER
 
 /**
- * @type {import('semantic-release').Options}
+ * @param {import('semantic-release').PluginSpec<unknown>[]} pluginsArray
  */
-export default {
-	extends: ['@halospv3/hce.shared-config'], // static/.releaserc.yml
-	plugins: [
-		// https://github.com/semantic-release/commit-analyzer#options
-		[
-			'@semantic-release/commit-analyzer',
-			{
-				// description: https://github.com/semantic-release/commit-analyzer#releaserules
-				// defaults: https://github.com/semantic-release/commit-analyzer/blob/master/lib/default-release-rules.js
-				releaseRules: [
-					{
-						type: '(build|chore|fix)',
-						scope: 'deps',
-						release: 'patch',
-					},
-				],
-			},
-		],
-		'@semantic-release/release-notes-generator',
-		'@semantic-release/changelog',
-		// https://github.com/semantic-release/npm#options
-		'@semantic-release/npm',
-		// https://github.com/semantic-release/git#options
-		[
-			'@semantic-release/git',
-			gitOptions
-		],
-		[
-			'@semantic-release/github',
-			{
-				assets: ['halospv3-hce.shared-*.tgz'],
-			},
-		],
-	],
-};
+function getGitIndex(pluginsArray) { return pluginsArray.findIndex(v => v[0] === '@semantic-release/git') };
+
+// #region NPM
+
+// assert it's not already in the plugin array
+ok(!hceSharedConfig.plugins.find(v => v[0] === '@semantic-release/npm'));
+const arr = [...hceSharedConfig.plugins];
+arr.splice(getGitIndex(arr) + 1, 0, ['@semantic-release/npm', {}])
+
+// #endregion NPM
+
+// #region GITHUB
+
+ok(hceSharedConfig.plugins);
+const githubIndex = hceSharedConfig.plugins.findIndex(v => v[0] === "@semantic-release/github");
+hceSharedConfig.plugins[githubIndex][1].assets = ['halospv3-hce.shared-*.tgz'];
+
+// #endregion github
+
+export default hceSharedConfig;
