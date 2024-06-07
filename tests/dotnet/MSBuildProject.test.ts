bdf5d32fe1cf8732fe247ea4302ae79b026b82d9
Noah Sherwin <noahrsherwin@gmail.com>
2024-06-06 22:01:21 -0700

    test: refactor and expand basic tests for MSBuildProject

--- tests/dotnet/MSBuildProject.test.ts
+++ tests/dotnet/MSBuildProject.test.ts
@@ -1,18 +1,37 @@
-import { ok } from 'node:assert';
-import { todo } from 'node:test';
+import { deepStrictEqual, ok } from 'node:assert';
+import { describe, it, todo } from 'node:test';
 import {
 	MSBuildEvaluatedProjects,
 	MSBuildProject,
 	MSBuildProjectPreDefinedProperties,
 } from '@halospv3/hce.shared-config/dotnet/MSBuildProject';
 
-ok(Array.isArray(MSBuildEvaluatedProjects) && MSBuildEvaluatedProjects.length === 0);
+await describe('MSBuildProject', async () => {
+	await describe('MSBuildEvaluatedProjects', async () => {
+		await it('is empty by default', () => {
+			ok(Array.isArray(MSBuildEvaluatedProjects) && MSBuildEvaluatedProjects.length === 0);
+		});
+	});
 
-await todo(MSBuildProject.name, (t) => {
-	t.todo('custom properties?');
+	await describe('MSBuildProject', async () => {
+		await it('has f', () => {
+			ok('evaluateProperties' in MSBuildProject);
+		})
+
+		await todo('custom properties?');
+	});
+
+	await describe('MSBuildPreDefinedProperties', () => {
+		deepStrictEqual(
+			MSBuildProjectPreDefinedProperties,
+			[
+				'TargetFramework',
+				'TargetFrameworks',
+				'RuntimeIdentifier',
+				'RuntimeIdentifiers'
+			]
+		);
+	});
 });
 
-ok(
-	Array.isArray(MSBuildProjectPreDefinedProperties) &&
-	MSBuildProjectPreDefinedProperties.length > 0
-);
+
