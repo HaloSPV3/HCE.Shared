edf5a14665fc060d322329b20c0a4e435a4ea7e5
Noah Sherwin <noahrsherwin@gmail.com>
2024-06-21 06:26:03 -0700

    refactor!: move MSBuildProjectProperties to MSBuildProject.ts
    Also...
    - replace MSBuildProjectPreDefinedProperties with MSBuildProject.MatrixProperties
    - add MSBuildProject.PredefinedProperties

--- tests/dotnet/MSBuildProjectProperties.test.ts
+++
@@ -1,9 +0,0 @@
-import { MSBuildProjectProperties } from '@halospv3/hce.shared-config/dotnet/MSBuildProjectProperties';
-import { ok } from 'node:assert/strict';
-import { describe, it } from 'node:test';
-
-await describe('MSBuildProjectProperties', async () => {
-    await it('exists', () => {
-        ok(MSBuildProjectProperties);
-    })
-});
