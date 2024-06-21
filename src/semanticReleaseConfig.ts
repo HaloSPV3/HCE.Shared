8b6edbfd78c220b1fa0f79774548b010ff24a93e
Noah Sherwin <noahrsherwin@gmail.com>
2024-06-21 07:32:33 -0700

    refactor: change semanticReleaseConfig.baseConfig to its dynamic type; use satisfies more often

--- src/semanticReleaseConfig.ts
+++ src/semanticReleaseConfig.ts
@@ -1,14 +1,19 @@
+import type { Options as SRExecOptions } from '@semantic-release/exec';
 import type { Options as GitOptions } from '@semantic-release/git';
 import type { Options as GithubOptions } from '@semantic-release/github';
 import type { Options, PluginSpec } from 'semantic-release';
 import { DefaultOptions } from './setupGitPluginSpec.js';
 
-export const defaultPlugins: readonly PluginSpec[] = [
+
+/**
+ * @satisfies { PluginSpec[] }
+ */
+export const defaultPlugins = [
 	'@semantic-release/commit-analyzer',
 	'@semantic-release/release-notes-generator',
 	'@semantic-release/npm',
 	'@semantic-release/github',
-];
+] satisfies readonly PluginSpec[];
 
 export const baseConfig = {
 	/** @see https://semantic-release.gitbook.io/semantic-release/usage/plugins#plugin-options-configuration */
@@ -26,26 +31,23 @@ export const baseConfig = {
 		["semantic-release-export-data", {}],
 		["@semantic-release/release-notes-generator", {}],
 		["@semantic-release/changelog", {}],
-		[
-			"@semantic-release/git",
-			DefaultOptions
-		] as PluginSpec<GitOptions>,
+		["@semantic-release/git", DefaultOptions as GitOptions] satisfies PluginSpec<GitOptions>,
 		// Arbitrary shell commands - https://github.com/semantic-release/exec
 		// hint: set 'prepareCmd' to`dotnet publish`.
 		//   Because this is sorted after @semantic-release / git, the new Git tag will
 		//   be visible to dotnet(and GitVersion).Dotnet artifacts will be
 		//   versioned accordingly.
 		// Plugins' Steps: https://github.com/semantic-release/semantic-release/blob/master/docs/extending/plugins-list.md
-		["@semantic-release/exec", {}],
+		["@semantic-release/exec", {} as SRExecOptions] satisfies PluginSpec<SRExecOptions>,
 		["@semantic-release/github",
 			{
 				assets: [{
 					path: './publish/*'
 				}]
-			}
-		] as PluginSpec<GithubOptions>
+			} as GithubOptions
+		] satisfies PluginSpec<GithubOptions>
 	]
-} as Options;
+} satisfies Options;
 
 /// (OPTIONAL) update static Version strings before Git plugin
 // https://github.com/jpoehnelt/semantic-release-replace-plugin
