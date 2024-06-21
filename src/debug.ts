94e171a44db404130336e359ad72a3c24956e5cd
Noah Sherwin <noahrsherwin@gmail.com>
2024-06-21 06:26:36 -0700

    refactor: scope debug to just our namespace

--- src/debug.ts
+++ src/debug.ts
@@ -2,7 +2,7 @@ import debug from 'debug';
 
 const _debug = debug('@halospv3/hce.shared-config');
 
-if (process.argv.some(v => v.includes('--debug')) || debug.enabled('*')) {
+if (process.argv.some(v => v.includes('--debug')) || debug.enabled('@halospv3/hce.shared-config')) {
     debug.enable(_debug.namespace);
 }
 
