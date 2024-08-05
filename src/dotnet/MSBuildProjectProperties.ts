f336071cfa1848ada570e7bc9fdacd48ae00664a
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-05 04:55:17 -0700

    refactor: update MSBuildProjectProperties
    - remove InstanceProperties
    - add GetFullPath
    - rename FullPath to MSBuildProjectFullPath
    - only assign properties in constructor.
    - remove redundant 'public' modifiers

--- src/dotnet/MSBuildProjectProperties.ts
+++ src/dotnet/MSBuildProjectProperties.ts
@@ -1,57 +1,92 @@
 import { existsSync } from 'node:fs';
 import { basename, isAbsolute, resolve } from 'node:path';
+import { CaseInsensitiveMap } from '../CaseInsensitiveMap.js';
 
+// todo: move this somewhere else.
 export type StringBoolean = "true" | "false";
 
 /**
  * Known properties. Additional properties may be added upon request.
+ * 
+ * todo: add Reserved properties, Well-Known properties, Common properties, and more. Maybe as sub classes.\
+ * See: 
+ * - [MSBuild Reserved and Well-known Properties](https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild-reserved-and-well-known-properties?view=vs-2022)
+ * - [Common MSBuild project properties](https://learn.microsoft.com/en-us/visualstudio/msbuild/common-msbuild-project-properties?view=vs-2022)
+ * - [Microsoft.NET.Sdk](https://learn.microsoft.com/en-us/dotnet/core/project-sdk/msbuild-props)
+ * - [Microsoft.NET.Sdk.Web](https://learn.microsoft.com/en-us/aspnet/core/razor-pages/web-sdk?view=aspnetcore-8.0&toc=%2Fdotnet%2Fnavigate%2Ftools-diagnostics%2Ftoc.json&bc=%2Fdotnet%2Fbreadcrumb%2Ftoc.json#properties)
+ * - [Microsoft.NET.Sdk.Razor](https://learn.microsoft.com/en-us/aspnet/core/razor-pages/sdk?view=aspnetcore-8.0&toc=%2Fdotnet%2Fnavigate%2Ftools-diagnostics%2Ftoc.json&bc=%2Fdotnet%2Fbreadcrumb%2Ftoc.json)
+ * - [Microsoft.NET.Sdk.Desktop](https://learn.microsoft.com/en-us/dotnet/core/project-sdk/msbuild-props-desktop)
  */
 export class MSBuildProjectProperties {
-    public static readonly InstanceProperties: readonly string[] = Object.freeze(Object.keys(MSBuildProjectProperties.prototype));
+    /**
+     * Resolve the given path is not absolute. If the path exists, it is returned. Else, an Error is thrown.
+     * @param path 
+     * @returns 
+     */
+    static GetFullPath(path: string) {
+        if (!isAbsolute(path))
+            path = resolve(path);
+        if (!existsSync(path))
+            throw new Error(`${basename(path)} could not be found at "${path}"`);
+        return path;
+    }
 
-    public constructor(fullPath: string, ...rest: string[]) {
-        this.FullPath = fullPath;
-        if (!isAbsolute(this.FullPath)) this.FullPath = resolve(this.FullPath);
-        if (!existsSync(this.FullPath))
-            throw new Error(
-                `Project ${basename(fullPath)} could not be found at "${this.FullPath}"`
-            );
-
-        // filter rest for only property names not yet defined.
-        const instanceMembersLowerCase: string[] = MSBuildProjectProperties.InstanceProperties.map(v => v.toLowerCase());
-        const remainingProps: Record<string, string>[] = [...rest].filter(v => {
-            const lc = v.toLowerCase()
-            if (instanceMembersLowerCase.includes(lc)) {
-                instanceMembersLowerCase.splice(instanceMembersLowerCase.indexOf(lc))
-                return false;
+    constructor(msbuildProjectFullPath: string, properties: CaseInsensitiveMap<string, string>) {
+        this.MSBuildProjectFullPath = MSBuildProjectProperties.GetFullPath(msbuildProjectFullPath);
+        const keys: string[] = [...properties.keys()];
+        function getAndForget(key: string): string | undefined {
+            const v: string | undefined = properties.get(key);
+            if (v !== undefined)
+                keys.splice(keys.indexOf(key));
+            return v;
+        }
+        this.AssemblyName = getAndForget("AssemblyName") ?? "";
+        this.Description = getAndForget("Description") ?? "";
+        this.RuntimeIdentifier = getAndForget("RuntimeIdentifier") ?? "";
+        this.RuntimeIdentifiers = getAndForget("RuntimeIdentifiers") ?? "";
+        this.TargetFramework = getAndForget("TargetFramework") ?? "";
+        this.TargetFrameworks = getAndForget("TargetFrameworks") ?? "";
+        this.Version = getAndForget("Version") ?? "";
+        this.VersionPrefix = getAndForget("VersionPrefix") ?? "";
+        this.VersionSuffix = getAndForget("VersionSuffix") ?? "";
+        // rest
+        for (const k of keys) {
+            const v = properties.get(k);
+            if (v !== undefined) {
+                Object.defineProperty(
+                    this,
+                    k,
+                    {
+                        value: v,
+                        writable: false,
+                        enumerable: true,
+                        configurable: true
+                    }
+                );
             }
-            return true;
-        }).map(v => {
-            return { [v]: "" } as Record<string, string>
-        });
-        Object.assign(this, remainingProps);
+        }
     }
 
     readonly [Property: string]: string;
 
-    public readonly FullPath: string = "";
+    readonly MSBuildProjectFullPath: string;
 
-    public readonly AssemblyName: string = "";
+    readonly AssemblyName: string;
 
     /**
      * A long description for the assembly.
      * If {@link NugetProperties.PackageDescription} is not specified, then this property is also used as the description of the package.
      */
-    public readonly Description: string = "";
+    readonly Description: string = "";
 
     /** Set Version -OR- VersionPrefix. */
-    public readonly Version: string = "";
+    readonly Version: string = "";
 
     /**
      * Set Version -OR- VersionPrefix.
      * @remarks Setting {@link NugetProperties.PackageVersion} overwrites {@link VersionPrefix}
      */
-    public readonly VersionPrefix: string = "";
+    readonly VersionPrefix: string = "";
 
     /**
      * The effect of this property on the package version depends on the values of the Version and VersionPrefix properties, as shown in the following table:
@@ -64,13 +99,13 @@ export class MSBuildProjectProperties {
      * | VersionPrefix and VersionSuffix | $(VersionPrefix)-$(VersionSuffix) |
      * @remarks Setting {@link PackageVersion} overwrites {@link VersionSuffix}
      */
-    public readonly VersionSuffix: string = "";
+    readonly VersionSuffix: string;
 
-    public readonly TargetFramework: string = "";
+    readonly TargetFramework: string;
 
-    public readonly TargetFrameworks: string = "";
+    readonly TargetFrameworks: string;
 
-    public readonly RuntimeIdentifier: string = "";
+    readonly RuntimeIdentifier: string;
 
-    public readonly RuntimeIdentifiers: string = "";
+    readonly RuntimeIdentifiers: string;
 }
