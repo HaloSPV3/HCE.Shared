3091eadf30a61a0a86bacc2bed8dbe333e3a9644
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-11 18:40:00 -0700

    style: fix semicolons, commas, indents

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,31 @@
 // import { default as moon } from "eslint-config-moon";
 // import { default as moonNode } from "eslint-config-moon/node";
 // import { FlatCompat } from "@eslint/eslintrc"
-import eslintConfig from '@halospv3/hce.shared-config/eslintConfig';
-import stylistic from '@stylistic/eslint-plugin';
-import { config } from 'typescript-eslint';
+import eslintConfig from '@halospv3/hce.shared-config/eslintConfig'
+import stylistic from '@stylistic/eslint-plugin'
+import { config } from 'typescript-eslint'
 
 export default config(
-    ...eslintConfig,
-    stylistic.configs['recommended-flat'],
-    {
-        languageOptions: {
-            parserOptions: {
-                sourceType: 'module',
-                // @ts-expect-error TS2322 Type '{ allowDefaultProjectForFiles: string[]; }' is not assignable to type 'boolean | undefined'.
-                EXPERIMENTAL_useProjectService: {
-                    allowDefaultProjectForFiles: [
-                        './*.js'
-                    ]
-                }
-            }
+  ...eslintConfig,
+  stylistic.configs['recommended-flat'],
+  {
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+        // @ts-expect-error TS2322 Type '{ allowDefaultProjectForFiles: string[]; }' is not assignable to type 'boolean | undefined'.
+        EXPERIMENTAL_useProjectService: {
+          allowDefaultProjectForFiles: [
+            './*.js',
+          ],
         },
-        files: [
-            'src/**/*.ts',
-            'tests/**/*.ts',
-            '*.js',
-            '*.mjs',
-            'packemon.config.ts'
-        ]
-    }
-);
+      },
+    },
+    files: [
+      'src/**/*.ts',
+      'tests/**/*.ts',
+      '*.js',
+      '*.mjs',
+      'packemon.config.ts',
+    ],
+  },
+)
