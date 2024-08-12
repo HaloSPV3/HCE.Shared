a087f30bc6dfd147a80b8f0eac5ef23b8f862bd7
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-11 18:39:09 -0700

    style: add @stylistic/eslint-plugin; fix @stylistic/quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,31 @@
 // import { default as moon } from "eslint-config-moon";
 // import { default as moonNode } from "eslint-config-moon/node";
 // import { FlatCompat } from "@eslint/eslintrc"
-import eslintConfig from "@halospv3/hce.shared-config/eslintConfig"
-import { config } from "typescript-eslint";
+import eslintConfig from '@halospv3/hce.shared-config/eslintConfig';
+import stylistic from '@stylistic/eslint-plugin';
+import { config } from 'typescript-eslint';
 
 export default config(
     ...eslintConfig,
+    stylistic.configs['recommended-flat'],
     {
         languageOptions: {
             parserOptions: {
-                sourceType: "module",
+                sourceType: 'module',
                 // @ts-expect-error TS2322 Type '{ allowDefaultProjectForFiles: string[]; }' is not assignable to type 'boolean | undefined'.
                 EXPERIMENTAL_useProjectService: {
                     allowDefaultProjectForFiles: [
-                        "./*.js"
+                        './*.js'
                     ]
                 }
             }
         },
         files: [
-            "src/**/*.ts",
-            "tests/**/*.ts",
-            "*.js",
-            "*.mjs",
-            "packemon.config.ts"
+            'src/**/*.ts',
+            'tests/**/*.ts',
+            '*.js',
+            '*.mjs',
+            'packemon.config.ts'
         ]
     }
 );
