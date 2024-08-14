516075d233b86cb7a28a4c116a0337fc71be61f5
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-14 04:08:09 -0700

    refactor(dotnet): update MSBuildProject imports
    - remove execFileSync, replace with exec
    - remove existsSync
    - add EOL
    - add getOwnPropertyDescriptors
    - change Dirent to type import
    - sort imports

--- src/dotnet/MSBuildProject.ts
+++ src/dotnet/MSBuildProject.ts
@@ -1,12 +1,12 @@
-import { exec, execFileSync } from 'node:child_process';
-import { Dirent } from 'node:fs';
+import { type } from 'arktype'
+import { exec } from 'node:child_process'
+import { type Dirent } from 'node:fs'
 import { readdir, realpath, stat } from 'node:fs/promises';
 import { dirname, isAbsolute, join, resolve } from 'node:path';
-import { MSBuildProjectProperties } from './MSBuildProjectProperties.js';
-import { NugetProjectProperties } from './NugetProjectProperties.js';
-import { type } from "arktype";
 import { CaseInsensitiveMap } from '../CaseInsensitiveMap.js';
 import { getOwnPropertyDescriptors } from '../utils/reflection.js';
+import { MSBuildProjectProperties } from './MSBuildProjectProperties.js';
+import { NugetProjectProperties } from './NugetProjectProperties.js';
 
 /**
  * See [MSBuild well-known item metadata](https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild-well-known-item-metadata).
