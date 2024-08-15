3f0b0231d87174df0638030847e0f35e6f6dc852
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-15 05:10:29 -0700

    test(dotnet): format MSBuildProjectProperties.test.ts

--- tests/dotnet/MSBuildProjectProperties.test.ts
+++ tests/dotnet/MSBuildProjectProperties.test.ts
@@ -1,22 +1,22 @@
-import { ok, strictEqual } from 'node:assert/strict';
-import { describe, it, todo } from 'node:test';
-import { MSBuildProjectProperties } from '@halospv3/hce.shared-config/dotnet/MSBuildProjectProperties';
+import { ok, strictEqual } from 'node:assert/strict'
+import { describe, it, todo } from 'node:test'
+import { MSBuildProjectProperties } from '@halospv3/hce.shared-config/dotnet/MSBuildProjectProperties'
 
 await describe('MSBuildProjectProperties', async (c0) => {
-    await it("has expected name", () => {
-        strictEqual(MSBuildProjectProperties.name, c0.name);
-    });
+  await it('has expected name', () => {
+    strictEqual(MSBuildProjectProperties.name, c0.name)
+  })
+  await it('exists', () => {
+    ok(MSBuildProjectProperties)
+  })
+  await it('has prototype', () => {
+    ok('prototype' in MSBuildProjectProperties)
+  })
+  await describe('AssemblyName', async () => {
     await it('exists', () => {
-        ok(MSBuildProjectProperties);
-    });
-    await it('has prototype', () => {
-        ok("prototype" in MSBuildProjectProperties);
-    });
-    await describe("AssemblyName", async () => {
-        await it("exists", () => {
-            strictEqual("AssemblyName" in MSBuildProjectProperties.prototype, true)
-        });
-    });
+      strictEqual('AssemblyName' in MSBuildProjectProperties.prototype, true)
+    })
+  })
 
-    await todo("test remaining members");
-});
+  await todo('test remaining members')
+})
