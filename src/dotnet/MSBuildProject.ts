96892772a8d7ec9c9d6ca038b9fbc7efc260a1cd
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-15 07:02:44 -0700

    refactor(dotnet.MSBP): remove EOL returns from `getTargets` results

--- src/dotnet/MSBuildProject.ts
+++ src/dotnet/MSBuildProject.ts
@@ -214,7 +214,7 @@ export class MSBuildProject {
   static async getTargets(projectPath: string, includeNonPublic = false): Promise<string[]> {
     return execAsync(`dotnet msbuild ${projectPath} -targets`,
     ).then((v) => {
-      const targets = v.stdout.split('\n')
+      const targets = v.stdout.split('\n').map(v => v.replace('\r', ''))
       if (includeNonPublic)
         return targets
       return targets.filter(v => !v.startsWith('_'))
