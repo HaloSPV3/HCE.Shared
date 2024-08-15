323d79ace00bc7f2a9a58ae7d9fc7a92748991d8
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-14 18:20:51 -0700

    style: format src/eslintConfig.ts
    - @stylistic/quotes
    - @stylistic/indent
    - @stylistic/comma-dangle
    - @stylistic/semi
    - @stylistic/eol-last

--- src/eslintConfig.ts
+++ src/eslintConfig.ts
@@ -1,86 +1,86 @@
-import eslint from "@eslint/js";
-import { type TSESLint } from "@typescript-eslint/utils";
-import jsonc from "eslint-plugin-jsonc";
-import globals from 'globals/globals.json' with { type: "json" }
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js'
+import { type TSESLint } from '@typescript-eslint/utils'
+import jsonc from 'eslint-plugin-jsonc'
+import globals from 'globals/globals.json' with { type: 'json' }
+import tseslint from 'typescript-eslint'
 
 // https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config
 // https://www.google.com/search?q=javascript+recurse+through+object+and+remove+undefined+properties
 
 const globalIgnores: TSESLint.FlatConfig.Config = {
-    name: "global ignores",
-    ignores: [
-        "_tsout/**/*",
-        "_tsout/*",
-        "_tsout/",
-        "cjs/**/*",
-        "cjs/*/*",
-        "cjs/*",
-        "mjs/**/*",
-        "mjs/*",
-        "mjs/",
-        "node_modules/**/*",
-        "**/node_modules/**/*"
-    ]
-};
+  name: 'global ignores',
+  ignores: [
+    '_tsout/**/*',
+    '_tsout/*',
+    '_tsout/',
+    'cjs/**/*',
+    'cjs/*/*',
+    'cjs/*',
+    'mjs/**/*',
+    'mjs/*',
+    'mjs/',
+    'node_modules/**/*',
+    '**/node_modules/**/*',
+  ],
+}
 
 export default tseslint.config(
-    {
-        name: "flat/recommended-with-json - https://github.com/ota-meshi/eslint-plugin-jsonc",
-        extends: jsonc.configs["flat/recommended-with-json"],
-        files: [
-            "*.json",
-            "**/*.json"
-        ],
-        ignores: globalIgnores.ignores
+  {
+    name: 'flat/recommended-with-json - https://github.com/ota-meshi/eslint-plugin-jsonc',
+    extends: jsonc.configs['flat/recommended-with-json'],
+    files: [
+      '*.json',
+      '**/*.json',
+    ],
+    ignores: globalIgnores.ignores,
+  },
+  {
+    name: 'flat/recommended-with-json5 - https://github.com/ota-meshi/eslint-plugin-jsonc',
+    extends: jsonc.configs['flat/recommended-with-json5'],
+    files: [
+      '*.json5',
+      '**/*.json5',
+    ],
+    ignores: globalIgnores.ignores,
+  },
+  {
+    name: 'flat/recommended-with-jsonc - https://github.com/ota-meshi/eslint-plugin-jsonc',
+    extends: jsonc.configs['flat/recommended-with-jsonc'],
+    files: [
+      '*.jsonc',
+      '**/*.jsonc',
+    ],
+    ignores: globalIgnores.ignores,
+  },
+  {
+    name: 'TSJS',
+    extends: [
+      eslint.configs.recommended as TSESLint.FlatConfig.Config,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
+    ],
+    files: [
+      '**/*.ts',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.js',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    ignores: globalIgnores.ignores,
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        // @ts-expect-error TS2322 Type '{ allowDefaultProjectForFiles: string[]; }' is not assignable to type 'boolean | undefined'.
+        EXPERIMENTAL_useProjectService: {
+          allowDefaultProjectForFiles: [
+            './*.js',
+          ],
+          maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 32,
+        },
+      },
+      globals: globals.node,
     },
-    {
-        name: "flat/recommended-with-json5 - https://github.com/ota-meshi/eslint-plugin-jsonc",
-        extends: jsonc.configs["flat/recommended-with-json5"],
-        files: [
-            "*.json5",
-            "**/*.json5"
-        ],
-        ignores: globalIgnores.ignores
-    },
-    {
-        name: "flat/recommended-with-jsonc - https://github.com/ota-meshi/eslint-plugin-jsonc",
-        extends: jsonc.configs["flat/recommended-with-jsonc"],
-        files: [
-            "*.jsonc",
-            "**/*.jsonc"
-        ],
-        ignores: globalIgnores.ignores
-    },
-    {
-        name: "TSJS",
-        extends: [
-            eslint.configs.recommended as TSESLint.FlatConfig.Config,
-            ...tseslint.configs.strict,
-            ...tseslint.configs.stylistic
-        ],
-        files: [
-            "**/*.ts",
-            "**/*.cts",
-            "**/*.mts",
-            "**/*.js",
-            "**/*.cjs",
-            "**/*.mjs"
-        ],
-        ignores: globalIgnores.ignores,
-        languageOptions: {
-            parserOptions: {
-                project: true,
-                // @ts-expect-error TS2322 Type '{ allowDefaultProjectForFiles: string[]; }' is not assignable to type 'boolean | undefined'.
-                EXPERIMENTAL_useProjectService: {
-                    allowDefaultProjectForFiles: [
-                        "./*.js"
-                    ],
-                    maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 32
-                }
-            },
-            globals: globals.node
-        }
-    },
-    globalIgnores
-);
+  },
+  globalIgnores,
+)
