6f4fdcbde3334a2020736fece064023963f8e9a3
Noah Sherwin <noahrsherwin@gmail.com>
2024-08-18 05:39:35 -0700

    test(dotnet): update MSBuildProject tests to src-compiled structure
    The source definitions are tested more thoroughly than the compiled definitions, but the results of the compiled definition would be the same as the source definition's results if the compiled definition is up-to-date.
    
    also, the code style was formatted.

--- tests/dotnet/MSBuildProject.test.ts
+++ tests/dotnet/MSBuildProject.test.ts
@@ -1,27 +1,33 @@
-import { deepStrictEqual, ok } from 'node:assert';
-import { describe, it } from 'node:test';
+import { deepStrictEqual, ok } from 'node:assert'
+import { describe, it } from 'node:test'
 import {
-	MSBuildProject,
-} from '@halospv3/hce.shared-config/dotnet/MSBuildProject';
+  MSBuildProject as exposed,
+} from '@halospv3/hce.shared-config/dotnet/MSBuildProject'
+import {
+  MSBuildProject as MSBP,
+} from '../../src/dotnet/MSBuildProject.js'
 
-await describe('MSBuildProject', async () => {
-	await describe('MatrixProperties', async (ctx1) => {
-		await it("has expected name", async () => {
-			ok(ctx1.name in MSBuildProject);
-		})
-		await it("is array of expected values", async () => {
-			deepStrictEqual(
-				MSBuildProject.MatrixProperties,
-				[
-					'TargetFramework',
-					'TargetFrameworks',
-					'RuntimeIdentifier',
-					'RuntimeIdentifiers'
-				]
-			);
-
-		})
-	});
-});
-
-
+await describe('MSBuildProject', async (c00) => {
+  await it('is exposed', () => {
+    deepStrictEqual(exposed.name, c00.name)
+  })
+  await it('is built', () => {
+    deepStrictEqual(exposed, MSBP)
+  })
+  await describe('MatrixProperties', async (ctx1) => {
+    await it('has expected name', async () => {
+      ok(ctx1.name in MSBP)
+    })
+    await it('is array of expected values', async () => {
+      deepStrictEqual(
+        MSBP.MatrixProperties,
+        [
+          'TargetFramework',
+          'TargetFrameworks',
+          'RuntimeIdentifier',
+          'RuntimeIdentifiers',
+        ],
+      )
+    })
+  })
+})
