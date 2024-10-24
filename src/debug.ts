96a9122d83fa4b27a352f6b983637385f96e49cf
Noah Sherwin <noahrsherwin@gmail.com>
2024-10-23 21:28:55 -0700

    style: format source files
    - stylistic/semi
    - stylistic/indent
    - stylistic/member-delimiter-style
    - stylistic/no-trailing-spaces
    - stylistic/quotes
    - stylistic/eol-last

--- src/debug.ts
+++ src/debug.ts
@@ -1,9 +1,9 @@
-import debug from 'debug';
+import debug from 'debug'
 
-const _debug = debug('@halospv3/hce.shared-config');
+const _debug = debug('@halospv3/hce.shared-config')
 
 if (process.argv.some(v => v.includes('--debug')) || debug.enabled('@halospv3/hce.shared-config')) {
-    debug.enable(_debug.namespace);
+  debug.enable(_debug.namespace)
 }
 
-export default _debug;
+export default _debug
