d80856c2b45d890ac6ac72cd71c55b378aa52d62
Noah Sherwin <noahrsherwin@gmail.com>
2024-11-09 01:18:39 -0800

    test(dotnet.MSBPP): simplify, rename test `throws if path does not exist`
    build(commitlint): add scope `dotnet.MSBPP`

--- tests/dotnet/MSBuildProjectProperties.test.ts
+++ tests/dotnet/MSBuildProjectProperties.test.ts
@@ -1,8 +1,8 @@
 import { deepStrictEqual, ok, strictEqual } from 'node:assert/strict'
 import { describe, it } from 'node:test'
+import { tmpNameSync } from 'tmp'
 import { MSBuildProjectProperties as MPP } from '../../src/dotnet/MSBuildProjectProperties.js'
 import { CaseInsensitiveMap } from './../../src/CaseInsensitiveMap.js'
-import { tmpNameSync } from 'tmp'
 
 await it('is built', async () =>
   deepStrictEqual(
@@ -14,15 +14,9 @@ await it('is built', async () =>
 await describe('MSBuildProjectProperties', async (c0) => {
   const emptyMap = new CaseInsensitiveMap<string, string>()
   const emptySample = new MPP('', emptyMap)
-  await it('throws if given path does not resolve to an existing path', () => {
-    try {
-      const tmpName = tmpNameSync()
-      new MPP(tmpName, emptyMap)
-      throw new Error(`MSBuildProjectProperties saw ${tmpName} and thought it existed when it shouldn't!`)
-    }
-    catch {
-      // good!
-    }
+  await it('throws if path does not exist', () => {
+    const tmpName = tmpNameSync()
+    new MPP(tmpName, emptyMap)
   })
   await it('does not throw if given path is empty string (defaults to CWD)', () => {
     strictEqual(emptySample.MSBuildProjectFullPath, process.cwd())
