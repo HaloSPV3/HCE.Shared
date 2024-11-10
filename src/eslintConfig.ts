5bc36f43cd7104cf84fe293296ff4ea4c8994dc3
Noah Sherwin <noahrsherwin@gmail.com>
2024-11-09 21:54:38 -0800

    build: update eslint configs

--- src/eslintConfig.ts
+++ src/eslintConfig.ts
@@ -70,13 +70,9 @@ export const _configArray: tseslint.ConfigWithExtends[] = [
     ignores: globalIgnores.ignores,
     languageOptions: {
       parserOptions: {
-        project: true,
-        // @ts-expect-error TS2322 Type '{ allowDefaultProjectForFiles: string[]; }' is not assignable to type 'boolean | undefined'.
-        EXPERIMENTAL_useProjectService: {
-          allowDefaultProjectForFiles: [
-            './*.js',
-          ],
-          maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 32,
+        projectService: {
+          allowDefaultProject: ['./*.js'],
+          loadTypeScriptPlugins: true,
         },
       },
       globals: globals.node,
@@ -85,63 +81,4 @@ export const _configArray: tseslint.ConfigWithExtends[] = [
   globalIgnores,
 ] satisfies tseslint.ConfigWithExtends[]
 
-export default tseslint.config(
-  {
-    name: 'flat/recommended-with-json - https://github.com/ota-meshi/eslint-plugin-jsonc',
-    extends: jsonc.configs['flat/recommended-with-json'],
-    files: [
-      '*.json',
-      '**/*.json',
-    ],
-    ignores: globalIgnores.ignores,
-  },
-  {
-    name: 'flat/recommended-with-json5 - https://github.com/ota-meshi/eslint-plugin-jsonc',
-    extends: jsonc.configs['flat/recommended-with-json5'],
-    files: [
-      '*.json5',
-      '**/*.json5',
-    ],
-    ignores: globalIgnores.ignores,
-  },
-  {
-    name: 'flat/recommended-with-jsonc - https://github.com/ota-meshi/eslint-plugin-jsonc',
-    extends: jsonc.configs['flat/recommended-with-jsonc'],
-    files: [
-      '*.jsonc',
-      '**/*.jsonc',
-    ],
-    ignores: globalIgnores.ignores,
-  },
-  {
-    name: 'TSJS',
-    extends: [
-      eslint.configs.recommended as TSESLint.FlatConfig.Config,
-      ...tseslint.configs.strict,
-      ...tseslint.configs.stylistic,
-    ],
-    files: [
-      '**/*.ts',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    ignores: globalIgnores.ignores,
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        // @ts-expect-error TS2322 Type '{ allowDefaultProjectForFiles: string[]; }' is not assignable to type 'boolean | undefined'.
-        EXPERIMENTAL_useProjectService: {
-          allowDefaultProjectForFiles: [
-            './*.js',
-          ],
-          maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 32,
-        },
-      },
-      globals: globals.node,
-    },
-  },
-  globalIgnores,
-)
+export default tseslint.config(..._configArray)
