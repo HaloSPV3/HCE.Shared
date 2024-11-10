1234251915dec1930666935f77d7624c9ec011eb
Noah Sherwin <noahrsherwin@gmail.com>
2024-11-09 21:51:14 -0800

    style: format prettier config, vscode settings

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,15 +1,15 @@
-import * as pcm from 'prettier-config-moon' with { type: 'json' };
+import * as pcm from 'prettier-config-moon' with { type: 'json' }
 export default {
-	...pcm,
-	...{
-		overrides: [
-			{
-				files: '*.json',
-				options: {
-					trailingComma: 'none',
-					bracketSameLine: true,
-				},
-			},
-		],
-	},
-};
+  ...pcm,
+  ...{
+    overrides: [
+      {
+        files: '*.json',
+        options: {
+          trailingComma: 'none',
+          bracketSameLine: true,
+        },
+      },
+    ],
+  },
+}
