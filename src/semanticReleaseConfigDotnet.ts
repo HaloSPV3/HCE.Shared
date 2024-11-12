9407b63d0e7e7057ab3c53ddfbd595a4b21dde9d
Noah Sherwin <noahrsherwin@gmail.com>
2024-11-11 18:56:41 -0800

    refactor(SemanticReleaseConfigDotnet): assign modified plugins array
    The .map result was being discarded.

--- src/semanticReleaseConfigDotnet.ts
+++ src/semanticReleaseConfigDotnet.ts
@@ -76,7 +76,7 @@ export class SemanticReleaseConfigDotnet {
   ) {
     this.options = baseConfig
     /* normalize PluginSpecs to tuples */
-    this.options.plugins.map(pluginSpec => typeof pluginSpec === 'string'
+    this.options.plugins = this.options.plugins.map(pluginSpec => typeof pluginSpec === 'string'
       ? [pluginSpec, {}]
       : pluginSpec,
     )
