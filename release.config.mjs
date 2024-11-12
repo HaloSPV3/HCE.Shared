3e818e389cb3eec6f6d3141f0a46114c3f3c662d
Noah Sherwin <noahrsherwin@gmail.com>
2024-11-11 19:30:38 -0800

    ci(release): overhaul release config
    release rules and behaviors should be unchanged, but the config...factory should process the config object more reliably. For developers, types will be more literal (i.e. const string value instead of string type)

--- release.config.mjs
+++ release.config.mjs
@@ -16,83 +16,139 @@
  */
 
 import hceSharedConfig from '@halospv3/hce.shared-config'
-import { ok } from 'node:assert/strict';
-import { inspect } from 'node:util';
+import { type } from 'arktype'
+import { ok } from 'node:assert/strict'
+import { env } from 'node:process'
+import { inspect } from 'node:util'
 
-const config = { ...(hceSharedConfig) };
+/**
+ * {@link hceSharedConfig} customized for this project's release pipeline
+ */
+const config = {
+  /** @type {import('ts-essentials').DeepWritable<typeof hceSharedConfig>} */
+  ...hceSharedConfig,
+  /** @type {import('semantic-release').PluginSpec[]} */
+  plugins: [...hceSharedConfig.plugins],
+  branches: [
+    ...hceSharedConfig.branches,
+    /**
+     * @type {const}
+     * @satisfies { import('semantic-release').BranchSpec }
+     */
+    // eslint-disable-next-line @stylistic/no-extra-parens
+    ({ name: 'next/3.0.0', prerelease: 'beta', range: '3.x' }),
+  ],
+}
 ok(config.plugins)
 
 console.debug(inspect(config, false, Infinity))
 
 // #region COMMIT ANALYZER
-const COMMIT_ANALYZER_ID = '@semantic-release/commit-analyzer';
+const COMMIT_ANALYZER_ID = '@semantic-release/commit-analyzer'
 const commitAnalyzerIndex = config.plugins.findIndex(pluginSpec =>
-	pluginSpec === COMMIT_ANALYZER_ID || pluginSpec[0] === COMMIT_ANALYZER_ID
-);
+  typeof pluginSpec === 'string'
+    ? pluginSpec === COMMIT_ANALYZER_ID
+    : pluginSpec[0] === COMMIT_ANALYZER_ID,
+)
 
-/** 
- * @type {import('semantic-release').PluginSpec<import('@semantic-release/commit-analyzer').CommitAnalyzerConfig>} 
- * https://github.com/semantic-release/commit-analyzer#options
- */
-let commitAnalyzer = config.plugins[commitAnalyzerIndex];
-if (typeof commitAnalyzer === 'string')
-	commitAnalyzer = [COMMIT_ANALYZER_ID, {}];
+/** @typedef {import('./src/semanticReleaseConfig.js').PluginSpecSRCommitAnalyzer} PluginSpecSRCommitAnalyzer */
 
-/* Set Release Rules */
+/** https://github.com/semantic-release/commit-analyzer#options */
+// eslint-disable-next-line @stylistic/no-extra-parens
+const commitAnalyzer = /** @type {Exclude<PluginSpecSRCommitAnalyzer,string> } */ (
+  typeof config.plugins[commitAnalyzerIndex] === 'string'
+    ? [COMMIT_ANALYZER_ID, {}]
+    : config.plugins[commitAnalyzerIndex]
+)
 
-let releaseRules = commitAnalyzer[1].releaseRules;
-if (releaseRules === undefined)
-	releaseRules = [];
-if (releaseRules instanceof String)
-	// @ts-expect-error Yes, it's a string. Stop complaining.
-	releaseRules = await import(commitAnalyzer[1].releaseRules)
-if (releaseRules === undefined || typeof releaseRules === 'string')
-	throw TypeError("it's supposed to be a string[] now");
+// Set Release Rules
+commitAnalyzer[1].releaseRules = commitAnalyzer[1].releaseRules !== undefined
+  ? type({
+    type: type.string.optional(),
+    scope: type.string.optional(),
+    release: type.string,
+  }).array().from(
+    typeof commitAnalyzer[1].releaseRules === 'string'
+      ? await import(commitAnalyzer[1].releaseRules)
+      : commitAnalyzer[1].releaseRules,
+  )
+  : []
 
-// @ts-expect-error Index signature in type 'readonly PluginSpec<any>[]' only permits reading. ts(2542)
-config.plugins[commitAnalyzerIndex] = commitAnalyzer;
+// assign modified tuple to commit analyzer
+config.plugins[commitAnalyzerIndex] = commitAnalyzer
 
 // #endregion COMMIT ANALYZER
 
 /**
  * @param {import('semantic-release').PluginSpec<unknown>[]} pluginsArray
  */
-function getGitIndex(pluginsArray) { return pluginsArray.findIndex(v => v[0] === '@semantic-release/git') };
+function getGitIndex(pluginsArray) {
+  return pluginsArray.findIndex(v =>
+    v[0] === '@semantic-release/git',
+  )
+};
 
 // #region NPM
 
 // assert it's not already in the plugin array
 if (undefined === config.plugins.find(v => v[0] === '@semantic-release/npm')) {
-	const plugins = [...config.plugins];
-	plugins.splice(getGitIndex(plugins) + 1, 0, ['@semantic-release/npm', {}])
-	config.plugins = plugins;
+  const plugins = [...config.plugins]
+  plugins.splice(getGitIndex(plugins) + 1, 0, ['@semantic-release/npm', {}])
+  config.plugins = plugins
 }
 
 // #endregion NPM
 
 // #region GITHUB
 
-ok(config.plugins);
-const githubIndex = config.plugins.findIndex(v => v[0] === "@semantic-release/github");
-config.plugins[githubIndex][1].assets = ['halospv3-hce.shared-*.tgz'];
+ok(config.plugins)
+
+const SRGHOptions = type({
+  githubUrl: type('undefined | string')
+    .default(() => env['GH_URL'] ?? env['GITHUB_URL']),
+  githubApiPathPrefix: type('undefined | string')
+    .default(() => env['GH_PREFIX'] ?? env['GITHUB_PREFIX']),
+  githubApiUrl: 'string?',
+  proxy: 'undefined | string | false',
+  assets: type.string.or(
+    type.string.or({
+      path: 'string',
+      name: 'string?',
+      label: 'string?',
+    }).array(),
+  ).optional(),
+  successComment: type('undefined | string | false')
+    .default('":tada: This issue has been resolved in version ${nextRelease.version} :tada:\n\nThe release is available on [GitHub release](<github_release_url>)"'),
+  failComment: 'undefined | string | false',
+  failTitle: 'string | false = "The automated release is failing 🚨"',
+  labels: type('string[] | false')
+    .default(() => ['semantic-release']),
+  assignees: 'unknown?',
+  releasedLabels: type('string[]')
+    .default(() => ['released<%= nextRelease.channel ? \\` on @\\${nextRelease.channel}\\` : "" %>']),
+  addReleases: 'false | "bottom" | "top" = false',
+  draftRelease: 'boolean = false',
+  releaseNameTemplate: type.string
+    .default('<%= nextRelease.notes %>'),
+  releaseBodyTemplate: type.string
+    .default('<%= nextRelease.notes %'),
+  discussionCategoryName: 'string | false = false',
+})
+
+const GHTuple = type(['"@semantic-release/github"', SRGHOptions])
+const github = config.plugins.find(v => GHTuple.allows(v))
+if (github)
+  github[1].assets = ['halospv3-hce.shared-*.tgz']
 
 /* modify branches */
 
 ok(config.branches)
 
 // isArray doesn't work well with readonly arrays
-if (typeof config.branches === 'string' || (!('find' in config.branches && 'findIndex' in config.branches))) {
-	config.branches = [config.branches];
+if (typeof config.branches === 'string' || !('find' in config.branches && 'findIndex' in config.branches)) {
+  config.branches = [config.branches]
 }
-
-/** @type { import('semantic-release').BranchSpec }*/
-const next_3xx = { name: 'next/3.0.0', prerelease: 'beta', range: '3.x' }
-config.branches = [
-	...config.branches,
-	next_3xx
-]
-
 
 // #endregion github
 
-export default config;
+export default config
