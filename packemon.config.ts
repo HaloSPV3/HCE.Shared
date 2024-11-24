af93194cd574e292c2d80550bbd694f60e730cab
Noah Sherwin <noahrsherwin@gmail.com>
2024-11-23 20:35:38 -0800

    chore: move JSON stringification out of template literal

--- packemon.config.ts
+++ packemon.config.ts
@@ -5,6 +5,6 @@ import type { ConfigFile } from 'packemon'
 export default {
   babelInput(config: BabelOptions) {
     const tmp = { ...config, caller: undefined, configFile: undefined }
-    writeFileSync('./babel.config.json', `${JSON.stringify(tmp, undefined, 2)}`, { encoding: 'utf8' })
+    writeFileSync('./babel.config.json', JSON.stringify(tmp, undefined, 2), { encoding: 'utf8' })
   },
 } as ConfigFile
