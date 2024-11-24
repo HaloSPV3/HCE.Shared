7306edfbdbf1b71c3c4958f22676b3bd7f6af348
Noah Sherwin <noahrsherwin@gmail.com>
2024-11-23 20:21:39 -0800

    chore: update prettier config for type validation

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,15 +1,26 @@
 import * as pcm from 'prettier-config-moon' with { type: 'json' }
-export default {
+
+/**
+ * @typedef {import('prettier').Config} Config
+ * @type {Config}
+ */
+const cfg = {
   ...pcm,
-  ...{
-    overrides: [
-      {
-        files: '*.json',
-        options: {
-          trailingComma: 'none',
-          bracketSameLine: true,
-        },
+  // @ts-expect-error string literal doesn't implicit cast to string enum
+  trailingComma: pcm.trailingComma,
+  // @ts-expect-error string literal doesn't implicit cast to string enum
+  proseWrap: pcm.proseWrap,
+  overrides: [
+    ...pcm.overrides,
+    {
+      files: ['*.json'],
+      options: {
+        trailingComma: 'none',
+        bracketSameLine: true,
       },
-    ],
-  },
+    },
+  ],
+
 }
+
+export default cfg
