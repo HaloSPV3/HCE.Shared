68fd4be382764a41fca23b68db6773872e435838
Noah Sherwin <noahrsherwin@gmail.com>
2025-01-02 19:38:56 -0800

    style(dotnet/MSBuildProject.projects): remove arrow functions's curly braces

--- tests/dotnet/MSBuildProject.projects.ts
+++ tests/dotnet/MSBuildProject.projects.ts
@@ -7,9 +7,7 @@ export const {
 } = await MSBP.PackableProjectsToMSBuildProjects([
   resolve(import.meta.dirname, '../../dotnet/samples/HCE.Shared.DeterministicNupkg/HCE.Shared.DeterministicNupkg.csproj'),
   resolve(import.meta.dirname, '../../dotnet/samples/HCE.Shared.SignAfterPack/HCE.Shared.SignAfterPack.csproj'),
-]).then((v: MSBP[]) => {
-  return Object.freeze({
-    DeterministicNupkgCsproj: Object.freeze(v[0]),
-    SignAfterPackCsproj: Object.freeze(v[1]),
-  })
-})
+]).then((v: MSBP[]) => Object.freeze({
+  DeterministicNupkgCsproj: Object.freeze(v[0]),
+  SignAfterPackCsproj: Object.freeze(v[1]),
+}))
