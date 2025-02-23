5f3a74526edbdb8c1ec5e17f8d49379c3633e4d3
Noah Sherwin <noahrsherwin@gmail.com>
2025-02-22 17:17:20 -0800

    test(dotnet.SemanticRelease): remove old "configDotnet" test suite

--- tests/semanticReleaseConfigDotnet.test.ts
+++ tests/semanticReleaseConfigDotnet.test.ts
@@ -1,7 +1,8 @@
 import { getConfig, SemanticReleaseConfigDotnet } from '../src/semanticReleaseConfigDotnet.js'
-import { deepStrictEqual, strictEqual } from 'node:assert/strict'
+import { deepStrictEqual, ok, strictEqual } from 'node:assert/strict'
 import { describe, it, todo } from 'node:test'
 import { isConstructor } from '../src/utils/reflection.js'
+import { join } from 'node:path'
 
 await it('is built', async () =>
   deepStrictEqual(
@@ -20,5 +21,27 @@ await describe('SemanticReleaseConfigDotnet', async () => {
 await describe('getConfig', async () => {
   await it('is a function', () => {
     strictEqual(typeof getConfig, 'function')
+  })
+  await it('does not throw when projectToPackAndPush contains at least one item', async () => {
+    process.env.GITHUB_REPOSITORY_OWNER = 'HaloSPV3'
+    process.env.SKIP_TOKEN = 'true'
+    const DeterministicNupkgCsprojPath = join(import.meta.dirname, '../dotnet/samples/HCE.Shared.DeterministicNupkg/HCE.Shared.DeterministicNupkg.csproj')
+    const actual = await getConfig([DeterministicNupkgCsprojPath])
+      .catch(v => v instanceof Error ? v : new Error(String(v)))
+
+    ok(!(actual instanceof Error), '`actual` should not be an Error.\n' + actual.stack)
+  })
+
+  await it('throws Error when projectsToPublish is an empty array.', async () => {
+    let actual = undefined
+    try {
+      actual = await getConfig([])
+    }
+    catch (error) {
+      actual = error as Error
+    }
+
+    ok(actual instanceof Error)
+    ok(actual.message.includes('projectsToPublish.length must be > 0 or PROJECTS_TO_PUBLISH must be defined and contain at least one path.'))
   })
 })
