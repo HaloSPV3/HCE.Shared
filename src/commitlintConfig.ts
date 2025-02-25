e88f45e66121568e4836bbb105e9a1573094375f
Noah Sherwin <noahrsherwin@gmail.com>
2025-02-24 21:37:34 -0800

    refactor(commitlintConfig): export config as const w/ satisfies and JSDoc typing

--- src/commitlintConfig.ts
+++ src/commitlintConfig.ts
@@ -1,13 +1,14 @@
 import { RuleConfigSeverity, type UserConfig } from '@commitlint/types'
 
-const commitlintConfig: UserConfig = {
+/** @type { UserConfig } */
+const commitlintConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [RuleConfigSeverity.Disabled],
     'footer-max-line-length': [RuleConfigSeverity.Disabled],
     'header-max-length': [RuleConfigSeverity.Disabled],
   },
-}
+} as const satisfies UserConfig
 
 // only `default` export is loaded via `extends` config key!
 export default commitlintConfig
