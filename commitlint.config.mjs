82c1963b9cad8fcd849f12c6de13f18f7a8176cd
Noah Sherwin <noahrsherwin@gmail.com>
2025-02-24 21:36:57 -0800

    chore: import commitlintConfig.ts into commitlint.config.mjs via TSX API

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,11 @@
-import commitlintConfig from '@halospv3/hce.shared-config/commitlintConfig'
+import { tsImport } from 'tsx/esm/api'
 import { RuleConfigSeverity } from '@commitlint/types'
 import vscodeSettingsJson from './.vscode/settings.json' with { type: 'json' }
+import { join } from 'path'
+
+/** @type {typeof import('./src/commitlintConfig.js').default} */
+const commitlintConfig = await tsImport('./src/commitlintConfig.ts', join(import.meta.dirname, 'src'))
+
 /** @typedef {import('./.vscode/settings.json.d.ts')} VscodeSettings */
 /** @typedef {import('@commitlint/types').RuleConfigCondition} RuleConfigCondition */
 // Remember to update the d.ts! This should be part of TypeScript, but is blocked by https://github.com/microsoft/TypeScript/issues/32063
@@ -13,6 +18,7 @@ const gitCommitMessageEditorScopes = Object.freeze(
 /** @type {import('@commitlint/types').UserConfig} */
 export default {
   ...commitlintConfig,
+  // get and add all commit scopes defined in ./.vscode/settings.json for extension 'gitCommitMessageEditor'
   rules: {
     'scope-enum': Object.freeze([
       RuleConfigSeverity.Error,
