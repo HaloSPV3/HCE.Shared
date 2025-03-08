c361258df701117545ba4006a2dc7ff3de7a60fe
Noah Sherwin <noahrsherwin@gmail.com>
2025-03-07 22:07:38 -0800

    chore: do not export local variable from eslint config

--- src/eslintConfig.ts
+++ src/eslintConfig.ts
@@ -24,7 +24,7 @@ const globalIgnores: TSESLint.FlatConfig.Config = {
   ],
 }
 
-export const _configArray: tseslint.ConfigWithExtends[] = [
+const _configArray: tseslint.ConfigWithExtends[] = [
   {
     name: 'flat/recommended-with-json - https://github.com/ota-meshi/eslint-plugin-jsonc',
     extends: jsonc.configs['flat/recommended-with-json'],
