c6e83022e3b546fe19adda79ab7e85f806256ccd
Noah Sherwin <noahrsherwin@gmail.com>
2025-03-15 22:03:54 -0700

    refactor(semantic-release): overhaul typings of semanticReleaseConfig.ts
    It is now flexible and mutable, but also literal (const-like).

--- src/semanticReleaseConfig.ts
+++ src/semanticReleaseConfig.ts
@@ -1,72 +1,73 @@
-import type { Options as SRCommitAnalyzerOptions } from '@semantic-release/commit-analyzer'
-import type { Options as SRExecOptions } from '@semantic-release/exec'
-import type { Options as SRGitOptions } from '@semantic-release/git'
-import type { Options as SRGithubOptions } from '@semantic-release/github'
-import type { Options, PluginSpec } from 'semantic-release'
-import { DefaultOptions } from './setupGitPluginSpec.js'
-import type { DeepWritable } from 'ts-essentials'
+import type { Options as SRCommitAnalyzerOptions } from '@semantic-release/commit-analyzer';
+import type { Options as SRExecOptions } from '@semantic-release/exec';
+import type { Options as SRGitOptions } from '@semantic-release/git';
+import type { Options as SRGithubOptions } from '@semantic-release/github';
+import type { Options, PluginSpec } from 'semantic-release';
+import { DefaultOptions } from './setupGitPluginSpec.js';
+
+export interface SRReleaseNotesGeneratorOptions {
+  /** conventional-changelog preset.
+   * @default 'angular'
+   */
+  preset?: 'angular' | 'atom' | 'codemirror' | 'ember' | 'eslint' | 'express' | 'jquery' | 'jscs' | 'jshint' | 'conventionalcommits' | (string & Record<never, never>) | undefined;
+  /** Requireable npm package with a custom conventional-changelog preset. */
+  config?: string | undefined;
+  /** Additional `conventional-changelog-parser` options that will overwrite ones loaded by `preset` or `config`. */
+  parserOpts: object;
+  /** Additional `conventional-changelog-writer` options that will overwrite ones loaded by `preset` or `config`. */
+  writerOpts: object;
+}
 
 /**
  * A two-type PluginSpec to tie a plugin's name to its Options type. This is
  * intended for use by plugins to associate their Options type with their plugin
- * name, but will anyone ever use this?
- * @augments PluginSpec<T>
+ * name.
  */
-export type PluginSpecTuple<P extends string, T> = P | [P, T]
-export type PluginSpecSRCommitAnalyzer = PluginSpecTuple<'@semantic-release/commit-analyzer', SRCommitAnalyzerOptions>
-export type PluginSpecSRExec = PluginSpecTuple<'@semantic-release/exec', SRExecOptions>
-export type PluginSpecSRGit = PluginSpecTuple<'@semantic-release/git', SRGitOptions>
-export type PluginSpecSRGithub = PluginSpecTuple<'@semantic-release/github', SRGithubOptions>
+export type PluginSpecTuple<P extends string, T> = [P, T];
+export type PluginSpecSRCommitAnalyzer<V extends SRCommitAnalyzerOptions = SRCommitAnalyzerOptions> = PluginSpecTuple<'@semantic-release/commit-analyzer', V>;
+export type PluginSpecSRChangelog<V extends Record<string, unknown>> = PluginSpecTuple<'@semantic-release/changelog', V>;
+export type PluginSpecExportData<V extends Record<string, unknown> = Record<string, unknown>> = PluginSpecTuple<'semantic-release-export-data', V>;
+export type PluginSpecSRReleaseNotesGen<V extends SRReleaseNotesGeneratorOptions = SRReleaseNotesGeneratorOptions> = PluginSpecTuple<'@semantic-release/release-notes-generator', V>;
+export type PluginSpecSRExec<V extends SRExecOptions = SRExecOptions> = PluginSpecTuple<'@semantic-release/exec', V>;
+export type PluginSpecSRGit<V extends SRGitOptions = SRGitOptions> = PluginSpecTuple<'@semantic-release/git', V>;
+export type PluginSpecSRGithub<V extends SRGithubOptions = SRGithubOptions> = PluginSpecTuple<'@semantic-release/github', V>;
 
-/**
- * @satisfies { readonly PluginSpec[] }
- */
+/** @satisfies { readonly PluginSpec[] } */
 export const defaultPlugins = Object.freeze([
   '@semantic-release/commit-analyzer',
   '@semantic-release/release-notes-generator',
   '@semantic-release/npm',
   '@semantic-release/github',
-] as const) satisfies readonly PluginSpec[]
+] as const) satisfies readonly PluginSpec[];
 
 // define as const for string literals in type, then...
 /** @see baseConfig */
 const _baseConfig = {
   /** @see https://semantic-release.gitbook.io/semantic-release/usage/plugins#plugin-options-configuration */
-  preset: 'conventionalcommits',
+  preset: 'conventionalcommits' as const,
   branches: [
     'main',
-    {
-      name: 'develop',
-      channel: 'develop',
-      prerelease: true,
-    },
-  ],
+    { name: 'develop', channel: 'develop', prerelease: true },
+  ] as ['main', { name: 'develop'; channel: 'develop'; prerelease: true }],
   plugins: [
-    ['@semantic-release/commit-analyzer' as const, {}] satisfies PluginSpecSRCommitAnalyzer,
-    ['semantic-release-export-data' as const, {}] satisfies PluginSpec<unknown>,
-    ['@semantic-release/release-notes-generator' as const, {}] satisfies PluginSpec<unknown>,
-    ['@semantic-release/changelog' as const, {}] satisfies PluginSpec<unknown>,
-    ['@semantic-release/git' as const, DefaultOptions] as const satisfies PluginSpecSRGit,
-    // Arbitrary shell commands - https://github.com/semantic-release/exec
-    // hint: set 'prepareCmd' to`dotnet publish`.
-    //   Because this is sorted after @semantic-release / git, the new Git tag will
-    //   be visible to dotnet(and GitVersion).Dotnet artifacts will be
-    //   versioned accordingly.
-    // Plugins' Steps: https://github.com/semantic-release/semantic-release/blob/master/docs/extending/plugins-list.md
-    ['@semantic-release/exec' as const, {}] satisfies PluginSpecSRExec,
-    [
-      '@semantic-release/github' as const,
-      {
-        addReleases: 'bottom',
-        assets: [{
-          path: './publish/*',
-        }],
-      } as const,
-    ] as const satisfies PluginSpecSRGithub,
-  ] as const,
-} as const satisfies Options
+    ['@semantic-release/commit-analyzer', {}],
+    ['semantic-release-export-data', {}],
+    ['@semantic-release/release-notes-generator', {}],
+    ['@semantic-release/changelog', {}],
+    ['@semantic-release/git', DefaultOptions],
+    ['@semantic-release/exec', {}],
+    ['@semantic-release/github', { addReleases: 'bottom', assets: [{ path: './publish/*' }] }],
+  ] as [
+    PluginSpecSRCommitAnalyzer,
+    PluginSpecExportData,
+    PluginSpecSRReleaseNotesGen,
+    PluginSpecSRChangelog<Record<string, unknown>>,
+    PluginSpecSRGit<typeof DefaultOptions>,
+    PluginSpecSRExec,
+    PluginSpecSRGithub<{ addReleases: 'bottom'; assets: [{ path: './publish/*' }] }>,
+  ] | PluginSpec[],
+};
 
-// ...assign with deep mutability
 /**
  * The base configuration for various Semantic Release scenarios.
  * - Prefers preset "conventionalcommits"
@@ -90,9 +91,10 @@ const _baseConfig = {
  *   - `@semantic-release/github`
  *     - uploads all files from `./publish/*`. This is non-recursive.
  *     - adds a list of links to related release pages (e.g. the release's page on npmjs.com)
+ *
+ * (OPTIONAL) update static Version strings before Git plugin
+ * - https://github.com/jpoehnelt/semantic-release-replace-plugin
+ * - https://github.com/droidsolutions/semantic-release-update-file
  * @satisfies {Options}
  */
-export const baseConfig = _baseConfig as DeepWritable<typeof _baseConfig>
-/// (OPTIONAL) update static Version strings before Git plugin
-// https://github.com/jpoehnelt/semantic-release-replace-plugin
-// https://github.com/droidsolutions/semantic-release-update-file
+export const baseConfig: typeof _baseConfig = _baseConfig satisfies Options;
