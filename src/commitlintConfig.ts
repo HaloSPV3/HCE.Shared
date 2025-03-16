502d382e25fb81aa72b87d6d12ace187ebfe44fe
Noah Sherwin <noahrsherwin@gmail.com>
2025-03-15 17:43:04 -0700

    refactor(commitlint): refactor typings, remove `as const`
    `as const` causes the undesired `readonly` attribute. Recursively. It also restricts arrays to const arrays. These annoyances outweigh the lazy typing benefits of `as const`.

--- src/commitlintConfig.ts
+++ src/commitlintConfig.ts
@@ -1,14 +1,23 @@
-import { RuleConfigSeverity, type UserConfig } from '@commitlint/types'
+import type { RuleConfigSeverity, UserConfig } from '@commitlint/types';
 
-/** @type { UserConfig } */
-const commitlintConfig = {
+// explicit type annotations for isolatedDeclarations. Allows for faster processing.
+
+const disabled = 0 as RuleConfigSeverity.Disabled;
+const commitlintConfig: UserConfig & {
+  extends: UserConfig['extends'] | ['@commitlint/config-conventional'];
+  rules: {
+    'body-max-line-length': [typeof disabled];
+    'footer-max-line-length': [typeof disabled];
+    'header-max-length': [typeof disabled];
+  };
+} = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [RuleConfigSeverity.Disabled],
-    'footer-max-line-length': [RuleConfigSeverity.Disabled],
-    'header-max-length': [RuleConfigSeverity.Disabled],
+    'body-max-line-length': [disabled],
+    'footer-max-line-length': [disabled],
+    'header-max-length': [disabled],
   },
-} as const satisfies UserConfig
+};
 
 // only `default` export is loaded via `extends` config key!
-export default commitlintConfig
+export default commitlintConfig;
