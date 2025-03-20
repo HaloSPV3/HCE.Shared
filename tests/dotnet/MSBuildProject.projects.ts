dbeffbb771ec250290506db26114f5afc4659eb8
Noah Sherwin <noahrsherwin@gmail.com>
2025-03-20 14:14:09 -0700

    test(dotnet): throw if either sample project cannot load; assign each to own exported const

--- tests/dotnet/MSBuildProject.projects.ts
+++ tests/dotnet/MSBuildProject.projects.ts
@@ -1,13 +1,24 @@
 import { resolve } from 'path';
 import { MSBuildProject as MSBP } from '../../src/dotnet/MSBuildProject.js';
 
-export const {
-  DeterministicNupkgCsproj,
-  SignAfterPackCsproj,
-} = await MSBP.PackableProjectsToMSBuildProjects([
-  resolve(import.meta.dirname, '../../dotnet/samples/HCE.Shared.DeterministicNupkg/HCE.Shared.DeterministicNupkg.csproj'),
-  resolve(import.meta.dirname, '../../dotnet/samples/HCE.Shared.SignAfterPack/HCE.Shared.SignAfterPack.csproj'),
+// Binding elements can't be exported directly with --isolatedDeclarations. ts(9019)
+const projects = await MSBP.PackableProjectsToMSBuildProjects([
+  resolve(
+    import.meta.dirname,
+    '../../dotnet/samples/HCE.Shared.DeterministicNupkg/HCE.Shared.DeterministicNupkg.csproj',
+  ),
+  resolve(
+    import.meta.dirname,
+    '../../dotnet/samples/HCE.Shared.SignAfterPack/HCE.Shared.SignAfterPack.csproj',
+  ),
 ]).then((v: MSBP[]) => Object.freeze({
-  DeterministicNupkgCsproj: Object.freeze(v[0]),
-  SignAfterPackCsproj: Object.freeze(v[1]),
-}));
+  DeterministicNupkgCsproj: Object.freeze(v.find(v => v.Properties.AssemblyName === 'HCE.Shared.DeterministicNupkg')
+    ?? (() => { throw new Error('Unable to find/load HCE.Shared.DeterministicNupkg.csproj'); })(),
+  ),
+  SignAfterPackCsproj: Object.freeze(v.find(v => v.Properties.AssemblyName === 'HCE.Shared.SignAfterPack')
+    ?? (() => { throw new Error('Unable to find/load HCE.Shared.SignAfterPack.csproj'); })(),
+  ),
+}),
+);
+export const DeterministicNupkgCsproj: Readonly<MSBP> = projects.DeterministicNupkgCsproj;
+export const SignAfterPackCsproj: Readonly<MSBP> = projects.SignAfterPackCsproj;
