f8c21091d88343d9a70a0e008556bd5755fdb3b7
Noah Sherwin <noahrsherwin@gmail.com>
2025-03-20 20:14:09 -0700

    build(eslintConfig): fix markdown-related errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,24 @@ import 'tsx';
 import { config } from 'typescript-eslint';
 
 /**
+ * @typedef {ReturnType<<T>(arg0:T)=> T extends (infer U)[] ? U : T>} FuncUnarray<T>
+ * @typedef {[{
+ *  name: "markdown/recommended";
+ *  files: ["**\/*.md"];
+ *  language: "markdown/commonmark";
+ *    plugins: {"@eslint/markdown": typeof import('@eslint/markdown').default};
+ *    rules: {
+ *      readonly "markdown/fenced-code-language": "error";
+ *      readonly "markdown/heading-increment": "error";
+ *      readonly "markdown/no-empty-links": "error";
+ *      readonly "markdown/no-invalid-label-refs": "error";
+ *      readonly "markdown/no-missing-label-refs": "error";
+ *    };
+ * }]}  markdownRecommended
+ */
+const mdRecommended =/** @type {[...markdownRecommended]} */ (md.configs.recommended);
+
+/**
  * @typedef {typeof import('./src/eslintConfig.ts').default} FlatConfigArray
  * @typedef {import('@stylistic/eslint-plugin').RuleOptions} RuleOptions
  * @type {FlatConfigArray} */
@@ -11,7 +29,8 @@ const hceSharedConfig = (await import('./src/eslintConfig.ts')).default;
 
 export default config(
   ...hceSharedConfig.filter(v => v.name == undefined || v.name !== 'JSTS'),
-  md.configs.recommended,
+  /** @satisfies {Parameters<typeof config>[0]} */
+  (mdRecommended),
   {
     extends: [
       hceSharedConfig.filter(v => v.name === 'JSTS'),
@@ -30,6 +49,13 @@ export default config(
           omitLastInOneLineClassBody: false,
         }),
       ],
+    },
+  },
+  {
+    name: 'stylistic excludes',
+    files: ['**/*.md'],
+    rules: {
+      '@stylistic/indent': 'off',
     },
   },
 );
