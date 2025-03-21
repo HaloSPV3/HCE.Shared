150d07f02b8a54f855db6278d9c8bce2fbe87bdd
Noah Sherwin <noahrsherwin@gmail.com>
2025-03-20 20:54:08 -0700

    chore: append final newline to babel config file

--- packemon.config.ts
+++ packemon.config.ts
@@ -12,7 +12,7 @@ export default {
         { ...config, caller: undefined, configFile: undefined } as BabelOptions,
         undefined,
         2,
-      ),
+      ) + '\n',
       'utf8',
     );
   },
