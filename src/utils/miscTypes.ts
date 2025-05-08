84ee8b9b12c13b76048da0c342ae379770440241
Noah Sherwin <noahrsherwin@gmail.com>
2025-05-08 16:14:12 -0700

    refactor(utils.miscTypes): add `Integer<N>`, `TupleIndices<T>`, `InstanceOrStatic`

--- src/utils/miscTypes.ts
+++ src/utils/miscTypes.ts
@@ -5,3 +5,13 @@ export type BooleanString = typeof tBooleanString.infer;
 
 export const tEmptyOrBooleanString = type(tBooleanString.or('""'));
 export type EmptyOrBooleanString = typeof tEmptyOrBooleanString.infer;
+
+export type Integer<N extends number> = `${N}` extends `${number}.${number}` ? never : N;
+
+/**
+ * @see https://stackoverflow.com/a/73920140/14894786
+ */
+export type TupleIndices<T extends readonly unknown[]> =
+  Extract<keyof T, `${number}`> extends `${infer N extends number}` ? N : never;
+
+export type InstanceOrStatic = 'Instance' | 'Static';
