e50ee6ffc955f0fc56927d34b5839df9e976a8fe
Noah Sherwin <noahrsherwin@gmail.com>
2025-05-08 14:47:50 -0700

    docs(eslintConfig): document `jsonCfgReducer`

--- src/eslintConfig.ts
+++ src/eslintConfig.ts
@@ -144,6 +144,12 @@ interface JsoncCfgReducerOut {
   >;
 }
 
+/**
+ * Merge erroneously mutually-exclusive configs from `eslint-plugin-jsonc`.
+ * @param {JsoncCfgReducerIn | JsoncCfgReducerOut} a A config exported by `eslint-plugin-jsonc` -OR- the output of this function.
+ * @param {JsoncCfgReducerIn | JsoncCfgReducerOut} b A config exported by `eslint-plugin-jsonc` -OR- the output of this function.
+ * @returns {JsoncCfgReducerOut} A merged combination of {@link a} and {@link b}.
+ */
 function jsonCfgReducer(
   a: JsoncCfgReducerIn | JsoncCfgReducerOut,
   b: JsoncCfgReducerIn | JsoncCfgReducerOut,
