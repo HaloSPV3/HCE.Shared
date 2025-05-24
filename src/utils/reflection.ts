a6aaadfe5c4aa3bbee6ef07e2af1d2ac3a4483d1
Noah Sherwin <noahrsherwin@gmail.com>
2025-05-24 14:38:47 -0700

    refactor(utils.reflection): re-export all reflection modules

--- src/utils/reflection.ts
+++ src/utils/reflection.ts
@@ -1,5 +1,11 @@
 export type * from './reflection/FunctionLike.js';
 export type * from './reflection/GetterDescriptor.js';
+export type * from './reflection/InstancePropertyDescriptorMap.js';
+export type * from './reflection/InstanceTypeOrSelfPropertyDescriptorMap.js';
+export type * from './reflection/OwnGetterDescriptorMap.js';
+export type * from './reflection/OwnKeyOf.js';
+export type * from './reflection/OwnPropertyDescriptorMap.js';
+export type * from './reflection/PropertyDescriptorMap.js';
 
 export * from './reflection/filterForGetters.js';
 export * from './reflection/getOwnPropertyDescriptors.js';
@@ -8,4 +14,5 @@ export * from './reflection/getPrototypeChainOf.js';
 export * from './reflection/getPrototypeOf.js';
 export * from './reflection/inheritance.js';
 export * from './reflection/isConstructor.js';
+export * from './reflection/isGetterDescriptor.js';
 export * from './reflection/listOwnGetters.js';
