e860eddccaea975cd115caec4ecf89ac6946e0e6
Noah Sherwin <noahrsherwin@gmail.com>
2025-05-24 14:18:19 -0700

    feat(utils.reflection.getOPDs): move `getOwnPropertyDescriptors` to its own file

--- src/utils/reflection.ts
+++ src/utils/reflection.ts
@@ -1,68 +1,10 @@
-import {
-  type BaseClassProto,
-  type ClassLike,
-  type ClassLike_Unknown,
-  type ConstructorConstraint,
-  type SuperClassLike,
-  type WithProto,
-} from './reflection/inheritance.js';
-import { isConstructor } from './reflection/isConstructor.js';
-
 export type * from './reflection/FunctionLike.js';
 export type * from './reflection/GetterDescriptor.js';
 
 export * from './reflection/filterForGetters.js';
 export * from './reflection/getOwnPropertyDescriptors.js';
+export * from './reflection/getPrototypeChainOf.js';
 export * from './reflection/getPrototypeOf.js';
 export * from './reflection/inheritance.js';
 export * from './reflection/isConstructor.js';
 export * from './reflection/listOwnGetters.js';
-
-/**
- * Iterate through the class and its base/super classes until an anonymous function is reached. This is the default superclass all classes extend.
- * @param classDef Any class type. This or its prototype are included in the return value.
- * @param [returnType] Default: 'classes'; Determines return type. If 'classInstances', the return type is an array of the classes' `.prototype`. Else, the classes themselves are returned.
- * @returns `returnType extends 'classInstances' ? ClassLike<T>[] : ClassLike<T>[].map(c => c.prototype)`
- * Excludes default superclasses e.g. anonymous functions, native code.
- * @since 3.0.0
- */
-export function getPrototypes<T extends ClassLike<ConstructorConstraint<T> & WithProto<SuperClassLike | BaseClassProto>>>(
-  classDef: ClassLike<T> | object,
-  returnType: 'classes' | 'classInstances' = 'classes',
-): typeof returnType extends 'classInstances' ? ClassLike<T>[] : ClassLike<T>['prototype'][] {
-  // class definitions or their respective .prototype; exclude default superclasses.
-  const returnValue: object[] = [];
-  let current: ClassLike<T> | ClassLike_Unknown | object = classDef;
-  let parent: ClassLike_Unknown | object | null = Reflect.getPrototypeOf(current);
-
-  while (undefined != parent) {
-    // current is a Class symbol/constructor. Object.getOwnPropertyDescriptors on current will include static properties.
-    if (isConstructor(current))
-      returnValue.push(returnType === 'classInstances' ? current.prototype as object : current);
-    else break;
-
-    /*
-     * Assign the super class to current.
-     * If the argument is a class, Object.getPrototypeOf method returns the
-     * superclass.
-     */
-    if (
-      undefined != (parent = Reflect.getPrototypeOf(current))
-      // && isConstructor(parent)
-
-      && 'name' in parent
-      && typeof parent.name === 'string'
-      && '' !== parent.name
-    ) {
-      current = parent;
-    }
-    else {
-      break;
-    }
-  }
-  return returnValue;
-  /*
-    assuming current is NugetProjectProperties...
-    Reflect.getPrototypeOf(current).name is 'MSBuildProjectProperties'
-   */
-}
---
+++ src/utils/reflection/getOwnPropertyDescriptors.ts
@@ -0,0 +1,52 @@
+import type { InstanceOrStatic } from '../miscTypes.js';
+import type { BaseClassProto, ClassLike, ConstructorConstraint, SuperClassLike, WithProto } from './inheritance.js';
+import type { InstancePropertyDescriptorMap } from './InstancePropertyDescriptorMap.js';
+import type { OwnPropertyDescriptorMap } from './OwnPropertyDescriptorMap.js';
+
+/**
+ * Get the property descriptors of the class or constructor similar to {@link Object.getOwnPropertyDescriptors}, but with more options--including recursion.
+ * @template {InstanceOrStatic} _InstanceOrStatic 'Instance' or 'Static'. Determines the return type.
+ * @param classDefinition A class or constructor, cast to {@link ClassLike}.
+ * @param instanceOrStatic 'Instance' or 'Static'. Determines the return type.
+ * @template {ClassLike<ConstructorConstraint<Class> & WithProto<SuperClassLike | BaseClassProto>>} Class
+ * The typeof {@link classDefinition}.
+ * {@link WithProto}<{@link SuperClassLike}> is necessary for walking the
+ * prototype chain for both Instance  and Static properties.
+ * If unspecified, the return type may be incorrect.
+ * @returns ({@link instanceOrStatic} === 'Instance') ? {@link InstancePropertyDescriptorMap<Class>}\
+ * : ({@link instanceOrStatic} === 'Static') ? {@link OwnPropertyDescriptorMap<Class>}\
+ * : never
+ * @throws {TypeError}
+ * @since 3.0.0
+ */
+export function getOwnPropertyDescriptors<
+  Class extends ClassLike<ConstructorConstraint<Class> & WithProto<SuperClassLike | BaseClassProto>>,
+  _InstanceOrStatic extends InstanceOrStatic,
+>(
+  classDefinition: Class,
+  instanceOrStatic: _InstanceOrStatic,
+): [_InstanceOrStatic] extends ['Instance']
+    ? InstancePropertyDescriptorMap<Class>
+    : [_InstanceOrStatic] extends ['Static']
+        ? OwnPropertyDescriptorMap<Class>
+        : never {
+  switch (instanceOrStatic) {
+    case 'Instance': {
+      return Object.getOwnPropertyDescriptors(classDefinition.prototype) as [Extract<typeof instanceOrStatic, 'Instance'>] extends ['Instance']
+        ? InstancePropertyDescriptorMap<Class>
+        : [_InstanceOrStatic] extends ['Static']
+            ? OwnPropertyDescriptorMap<Class>
+            : never;
+    }
+    case 'Static': {
+      return Object.getOwnPropertyDescriptors(classDefinition) as [Extract<typeof instanceOrStatic, 'Static'>] extends ['Instance']
+        ? InstancePropertyDescriptorMap<Class>
+        : [_InstanceOrStatic] extends ['Static']
+            ? OwnPropertyDescriptorMap<Class>
+            : never;
+    }
+    default: {
+      throw new TypeError('Argument `instanceOrStatic` must be "Instance" or "Static".');
+    }
+  }
+}
