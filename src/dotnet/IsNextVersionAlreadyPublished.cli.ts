970acb34ded0db20d5fa692f8e731ccce7bc1d61
Noah Sherwin <noahrsherwin@gmail.com>
2025-06-13 13:11:35 -0700

    feat(dotnet.INVAP, dotnet.NRI): add `GetIsNextVersionAlreadyPublishedCommand`, `getGithubOutput`, `getGithubOutputSync`, script for deferred execution of `IsNextVersionAlreadyPublished`
    build(deps): add `chardet`
    
    This is intended for GitHub Runners CI/CD, but may work on other hosts or locally.

---
+++ src/dotnet/IsNextVersionAlreadyPublished.cli.ts
@@ -0,0 +1,39 @@
+import { NugetRegistryInfo, getGithubOutput } from './NugetRegistryInfo.js'
+
+const args = process.argv.slice(2)
+
+// Parse command-line arguments - https://stackoverflow.com/a/76298476/14894786
+const options: {
+  packageId?: string
+  url?: string
+} & Record<string, string> = {}
+for (let i = 0; i < args.length; i += 2) {
+  const argName = args[i]
+  const argValue = args[i + 1]
+  if (argName.startsWith('--')) {
+    options[argName.slice(2)] = argValue
+  }
+}
+
+if (typeof options.packageId !== 'string')
+  throw new Error('packageId must be a string')
+if (typeof options.url !== 'string')
+  throw new Error('url must be a string')
+
+const packageId = options.packageId,
+  url = options.url,
+  // versionPattern = new RegExp(/(?<=\\n?new-release-version=)[\d.\-+\w]+(?=\n|$)/)
+  versionPattern = new RegExp(/[\d.\-+\w]+/)
+const ghOutput = await getGithubOutput() ?? {}
+const matches = versionPattern.exec(ghOutput['new-release-version'] ?? '')
+if (matches === null || matches.length === 0)
+  throw new Error('The variable new-release-version is not present in the GITHUB_OUTPUT env file or its value contains invalid characters.')
+
+const nextVersion = matches[0]
+const isPublished = await NugetRegistryInfo.IsNextVersionAlreadyPublished(url, packageId, nextVersion)
+
+if (typeof isPublished !== 'boolean')
+  throw new Error('isPublished is not a boolean')
+if (isPublished)
+  throw new Error(`${packageId}@${nextVersion} already exists at ${url}.`)
+console.log(`OK: ${packageId}@${nextVersion} does NOT yet exist at ${url}. Yay.`)
