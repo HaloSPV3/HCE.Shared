57207b8b359e02539aca4f93eb4518168ab3cf94
Noah Sherwin <noahrsherwin@gmail.com>
2025-06-27 16:04:36 -0700

    test(utils.miscTypes): add tests for type `TupleIndices`

--- tests/utils/miscTypes.test.ts
+++ tests/utils/miscTypes.test.ts
@@ -1,9 +1,11 @@
+import { deepStrictEqual } from 'node:assert';
+import { ok } from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import {
   tBooleanString,
   tEmptyOrBooleanString,
+  type TupleIndices,
 } from '../../src/utils/miscTypes.js';
-import { ok } from 'node:assert/strict';
 
 await describe('tBooleanString', async () => {
   await it('matches literal string "true"', () => {
@@ -38,4 +40,30 @@ await describe('tEmptyOrBooleanString', async () => {
   });
 });
 
-await describe('TupleIndices', { todo: true });
+await describe('TupleIndices', async () => {
+  await it('validates indices of tuple(2)', () => {
+    const arr = [1, 2] as const;
+    type ArrIndices = TupleIndices<typeof arr>;
+    // type ArrIndices = 0 | 1
+    const _0: ArrIndices = 0;
+    const _1: ArrIndices = 1;
+    const indices = [...arr.keys()];
+
+    deepStrictEqual(indices, [_0, _1]);
+    deepStrictEqual(indices.map(v => isValidIndexOf(v, indices)), [true, true]);
+  });
+
+  await it('validates indices of tuple(10)', () => {
+    const tenNines = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9] as const;
+    type IndicesOfTenNines = TupleIndices<typeof tenNines>;
+    // type IndicesOfNineNines = 0 | 1 | 2 | 9 | 3 | 4 | 5 | 6 | 7 | 8
+    const actual = [...tenNines.keys()] as TupleIndices<typeof tenNines>[];
+    const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] as const satisfies IndicesOfTenNines[];
+    deepStrictEqual(actual, expected);
+  });
+});
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+function isValidIndexOf(index: number, array: readonly unknown[]): index is TupleIndices<typeof array> {
+  return array[index] !== undefined;
+}
