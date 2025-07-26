44960a66c7f15c3af731715668e77cc9bbf0c465
Noah Sherwin <noahrsherwin@gmail.com>
2025-07-25 20:24:21 -0700

    build: remove type import from `@babel/core`; it's gone, Jim
    Why was so much of Babel's distributed type declarations gutted?

--- packemon.config.ts
+++ packemon.config.ts
@@ -1,15 +1,14 @@
-import type { TransformOptions as BabelOptions } from '@babel/core';
 import { writeFileSync } from 'node:fs';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import type { ConfigFile } from 'packemon';
 
 export default {
-  babelInput(config: BabelOptions) {
+  babelInput(config: Record<string, unknown>) {
     writeFileSync(
       './babel.config.json',
       JSON.stringify(
-        { ...config, caller: undefined, configFile: undefined } as BabelOptions,
+        { ...config, caller: undefined, configFile: undefined },
         undefined,
         2,
       ) + '\n',
