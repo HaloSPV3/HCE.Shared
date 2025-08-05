5006ab1d6ff46554f5fd17d076465fcc1e2018e6
Noah Sherwin <noahrsherwin@gmail.com>
2025-08-05 15:15:31 -0700

    fix(dotnet.INVAP): execute INVAP cli only if it's the main module
    fixes #455

--- src/dotnet/IsNextVersionAlreadyPublished.cli.ts
+++ src/dotnet/IsNextVersionAlreadyPublished.cli.ts
@@ -1,44 +1,61 @@
 import { NugetRegistryInfo, getGithubOutput } from './NugetRegistryInfo.js';
+import esMain from 'es-main';
 
-const args = process.argv.slice(2);
-
-// Parse command-line arguments - https://stackoverflow.com/a/76298476/14894786
-const options: {
-  packageId?: string;
-  source?: string;
-} & Record<string, string> = {};
-for (let i = 0; i < args.length; i += 2) {
-  const argName = args[i];
-  const argValue = args[i + 1];
-  if (argValue !== undefined && argName?.startsWith('--') === true && options[argName.slice(2)] !== undefined) {
-    options[argName.slice(2)] = argValue;
+/**
+ * @returns if successful
+ * @throws if...
+ * - {@link process.argv} does not include...
+ *   - `--packageId [string]`
+ *   - `--source [string]`
+ * - Value of `await getGithubOutput())['new-release-version']`...
+ *   - is not a valid Version
+ *   - already exists at `source`
+ */
+async function main(): Promise<0> {
+  const args = process.argv.slice(2);
+  // Parse command-line arguments - https://stackoverflow.com/a/76298476/14894786
+  const options: {
+    packageId?: string;
+    source?: string;
+  } & Record<string, string> = {};
+  for (let i = 0; i < args.length; i += 2) {
+    const argName = args[i];
+    const argValue = args[i + 1];
+    if (argValue !== undefined && argName?.startsWith('--') === true && options[argName.slice(2)] !== undefined) {
+      options[argName] = argValue;
+    }
   }
-}
 
-if (typeof options.packageId !== 'string')
-  throw new Error('packageId must be a string');
-if (typeof options.source !== 'string')
-  throw new Error('source must be a string');
+  if (typeof options.packageId !== 'string')
+    throw new Error('packageId must be a string');
+  if (typeof options.source !== 'string')
+    throw new Error('source must be a string');
 
-const packageId = options.packageId,
-  source = options.source,
-  versionPattern = new RegExp(/\d+\.\d+\.\d+([-+].+)?/);
-const ghOutput = await getGithubOutput() ?? {};
-const matches = versionPattern.exec(ghOutput['new-release-version'] ?? '');
-if (matches === null || matches.length === 0)
-  throw new Error(
-    'The variable new-release-version is not present in the GITHUB_OUTPUT env file or its value contains invalid characters.',
+  const packageId = options.packageId,
+    source = options.source,
+    versionPattern = new RegExp(/\d+\.\d+\.\d+([-+].+)?/);
+  const ghOutput = await getGithubOutput() ?? {};
+  const matches = versionPattern.exec(ghOutput['new-release-version'] ?? '');
+  if (matches === null || matches.length === 0)
+    throw new Error(
+      'The variable new-release-version is not present in the GITHUB_OUTPUT env file or its value contains invalid characters.',
+    );
+
+  const nextVersion = matches[0];
+  const isPublished = await NugetRegistryInfo.IsNextVersionAlreadyPublished(
+    source,
+    packageId,
+    nextVersion,
   );
 
-const nextVersion = matches[0];
-const isPublished = await NugetRegistryInfo.IsNextVersionAlreadyPublished(
-  source,
-  packageId,
-  nextVersion,
-);
+  if (typeof isPublished !== 'boolean')
+    throw new Error('isPublished is not a boolean');
+  if (isPublished)
+    throw new Error(`${packageId}@${nextVersion} already exists at ${source}.`);
+  console.log(`OK: ${packageId}@${nextVersion} does NOT yet exist at ${source}. Yay.`);
 
-if (typeof isPublished !== 'boolean')
-  throw new Error('isPublished is not a boolean');
-if (isPublished)
-  throw new Error(`${packageId}@${nextVersion} already exists at ${source}.`);
-console.log(`OK: ${packageId}@${nextVersion} does NOT yet exist at ${source}. Yay.`);
+  return 0;
+}
+
+if (esMain(import.meta))
+  await main();
