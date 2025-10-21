6a6559956ecbb20c06741c7e110b09af6aea52cc
Noah Sherwin <noahrsherwin@gmail.com>
2025-10-21 02:42:35 -0700

    build: fix bad import in ESLint config
    The Json plugin has a bad export which causes the default export to not be the default import.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,15 +1,17 @@
-import js from "@eslint/js";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
+import js from '@eslint/js';
+import _json from '@eslint/json';
+import markdown from '@eslint/markdown';
 import jsdoc from 'eslint-plugin-jsdoc';
 import unicorn from 'eslint-plugin-unicorn';
-import { defineConfig } from "eslint/config";
-import globals from "globals";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 import 'tsx';
-import tseslint from "typescript-eslint";
-import type { Linter } from "eslint";
+import tseslint from 'typescript-eslint';
+import type { Linter } from 'eslint';
 
 const { default: hceSharedConfig } = await import('./src/eslintConfig.ts');
+
+const json = 'default' in _json ? _json.default as typeof _json : _json;
 
 const unicornRecommended: Linter.Config = {
   ...unicorn.configs.recommended,
@@ -54,19 +56,19 @@ const jsdocRecommended_ts = {
     '**/*.tsx',
   ],
 };
-const config:Linter.Config[] = defineConfig(
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+const config: Linter.Config[] = defineConfig(
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
   ...hceSharedConfig,
   tseslint.configs.recommended,
   unicornRecommended,
   jsdocRecommended_js,
   jsdocRecommended_ts,
-  { files: ["**/*.md"], plugins: { markdown }, language: 'markdown/gfm', extends:[markdown.configs.recommended] },
+  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: [markdown.configs.recommended] },
   { name: 'stylistic excludes', files: ['**/*.md'], rules: { '@stylistic/indent': 'off' } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: [json.configs.recommended] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: [json.configs.recommended] },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: [json.configs.recommended] },
-  {ignores:['.yarn/sdks/**']}
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: [json.configs.recommended] },
+  { files: ['**/*.jsonc'], plugins: { json }, language: 'json/jsonc', extends: [json.configs.recommended] },
+  { files: ['**/*.json5'], plugins: { json }, language: 'json/json5', extends: [json.configs.recommended] },
+  { ignores: ['.yarn/sdks/**'] },
 );
 
 export default config;
