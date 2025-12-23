bc57b6230be63d504adca1f3717878c5ec752db6
Noah Sherwin <noahrsherwin@gmail.com>
2025-12-22 23:43:07 -0800

    build(dotnet.samples.AMT): init project; add commit scope

---
+++ dotnet/samples/HCE.Shared.AvaloniaMultiTarget/HCE.Shared.AvaloniaMultiTarget/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HCE.Shared.AvaloniaMultiTarget.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
