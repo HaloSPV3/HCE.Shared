e77b2f5bbebcc01ef99983c7fbfa735d7593c5e2
Noah Sherwin <noahrsherwin@gmail.com>
2026-04-22 17:16:17 -0700

    refactor: disable @typescript-eslint/no-unnecessary-type-assertion in Reflection functions

--- src/utils/reflection/getPrototypeOf.ts
+++ src/utils/reflection/getPrototypeOf.ts
@@ -8,5 +8,6 @@ import type { ProtoOrSuperClass, WithProto } from './inheritance.ts';
 export function getPrototypeOf<
   T extends WithProto<ProtoOrSuperClass>,
 >(object: T): T['__proto__'] {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   return Reflect.getPrototypeOf(object) as T['__proto__'];
 }
