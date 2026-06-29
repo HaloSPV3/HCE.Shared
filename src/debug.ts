f90753c9da257c5301f26c2ee2539e69ca53c54c
Noah Sherwin <noahrsherwin@gmail.com>
2026-06-28 21:30:13 -0700

    refactor: copy and modify `debug` types for accuracy

--- src/debug.ts
+++ src/debug.ts
@@ -1,8 +1,19 @@
-// note: @types/debug is incorrect. There is no .log function!
-import createDebugger, { type Debugger } from 'debug';
+import createDebugger from 'debug';
 import * as process from 'node:process';
 
-const _debug = createDebugger('@halospv3/hce.shared-config') as Debugger & { log: never };
+interface Debugger {
+  (formatter: unknown, ...arguments_: unknown[]): void;
+  color: string;
+  diff: number;
+  enabled: boolean;
+  namespace: string;
+  destroy: () => boolean;
+  extend: (namespace: string, delimiter?: string) => Debugger;
+}
+
+// note: @types/debug is incorrect. There is no .log function!
+// Omit<T,K> breaks the Function typing.
+const _debug: Debugger = createDebugger('@halospv3/hce.shared-config');
 
 if (process.argv.some(v => v.includes('--debug')) || createDebugger.enabled('@halospv3/hce.shared-config')) {
   createDebugger.enable(_debug.namespace);
